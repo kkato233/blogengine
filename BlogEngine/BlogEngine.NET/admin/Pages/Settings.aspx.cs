46945c4be5f18a81da6ae431b313d928fd39143b
SND\Oppositional_cp <SND\Oppositional_cp@localhost>
2007-05-03 18:34:10 +0000

    Modified settings administration to manage syndication specific settings.

--- BlogEngine/BlogEngine.NET/admin/Pages/Settings.aspx.cs
+++ BlogEngine/BlogEngine.NET/admin/Pages/Settings.aspx.cs
@@ -1,14 +1,17 @@
-#region Using
-
+/****************************************************************************
+Modification History:
+*****************************************************************************
+Date		Author		Description
+*****************************************************************************
+05/03/2007  brian.kuhn  Added syndication settings controls/logic
+****************************************************************************/
 using System;
-using System.Net.Mail;
 using System.IO;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
-
-#endregion
+using System.Web.UI.WebControls;
 
 using BlogEngine.Core;
 
@@ -50,14 +53,18 @@ public partial class admin_Pages_configuration : System.Web.UI.Page
 
   private void btnSave_Click(object sender, EventArgs e)
   {
-    // Basic
+    //-----------------------------------------------------------------------
+    // Set Basic settings
+    //-----------------------------------------------------------------------
     BlogSettings.Instance.Name = txtName.Text;
     BlogSettings.Instance.Description = txtDescription.Text;
     BlogSettings.Instance.PostsPerPage = int.Parse(txtPostsPerPage.Text);
     BlogSettings.Instance.Theme = ddlTheme.SelectedValue;
-    BlogSettings.Instance.EnableRelatedPosts = cbShowRelatedPosts.Checked;    
+    BlogSettings.Instance.EnableRelatedPosts = cbShowRelatedPosts.Checked;
 
-    // Email
+    //-----------------------------------------------------------------------
+    // Set Email settings
+    //-----------------------------------------------------------------------
     BlogSettings.Instance.Email = txtEmail.Text;
     BlogSettings.Instance.SmtpServer = txtSmtpServer.Text;
     BlogSettings.Instance.SmtpServerPort = int.Parse(txtSmtpServerPort.Text);
@@ -65,41 +72,75 @@ public partial class admin_Pages_configuration : System.Web.UI.Page
     BlogSettings.Instance.SmtpPassword = txtSmtpPassword.Text;
     BlogSettings.Instance.SendMailOnComment = cbComments.Checked;
 
-    // Comments
+    //-----------------------------------------------------------------------
+    // Set Comments settings
+    //-----------------------------------------------------------------------
     BlogSettings.Instance.IsCommentsEnabled = cbEnableComments.Checked;
     BlogSettings.Instance.EnableCountryInComments = cbEnableCountryInComments.Checked;
     BlogSettings.Instance.IsCoCommentEnabled = cbEnableCoComment.Checked;
     BlogSettings.Instance.ShowLivePreview = cbShowLivePreview.Checked;
     BlogSettings.Instance.DaysCommentsAreEnabled = int.Parse(ddlCloseComments.SelectedValue);
 
-    // Advanced
+    //-----------------------------------------------------------------------
+    // Set Advanced settings
+    //-----------------------------------------------------------------------
     BlogSettings.Instance.EnableHttpCompression = cbEnableCompression.Checked;
     BlogSettings.Instance.EnableSearchHightlight = cbEnableSearchHighlight.Checked;
     BlogSettings.Instance.RemoveWhitespaceInStyleSheets = cbRemoveWhitespaceInStyleSheets.Checked;
     BlogSettings.Instance.EnableOpenSearch = cbEnableOpenSearch.Checked;
     BlogSettings.Instance.MarkExternalLinks = cbMarkExternalLinks.Checked;
 
+    //-----------------------------------------------------------------------
+    // Set Syndication settings
+    //-----------------------------------------------------------------------
+    BlogSettings.Instance.SyndicationFormat = ddlSyndicationFormat.SelectedValue;
+
+    BlogSettings.Instance.AuthorName        = txtDublinCoreCreator.Text;
+    BlogSettings.Instance.Language          = txtDublinCoreLanguage.Text;
+
+    float latitude;
+    if (Single.TryParse(txtGeocodingLatitude.Text, out latitude))
+    {
+        BlogSettings.Instance.GeocodingLatitude     = latitude;
+    }
+    float longitude;
+    if (Single.TryParse(txtGeocodingLongitude.Text, out longitude))
+    {
+        BlogSettings.Instance.GeocodingLongitude    = longitude;
+    }
+
+    BlogSettings.Instance.Endorsement       = txtBlogChannelBLink.Text;
+
+    //-----------------------------------------------------------------------
+    //  Persist settings
+    //-----------------------------------------------------------------------
     BlogSettings.Instance.Save();
     Response.Redirect(Request.RawUrl, true);
   }
 
   private void BindSettings()
   {
-    // Basic
+    //-----------------------------------------------------------------------
+    // Bind Basic settings
+    //-----------------------------------------------------------------------
     txtName.Text = BlogSettings.Instance.Name;
     txtDescription.Text = BlogSettings.Instance.Description;
     txtPostsPerPage.Text = BlogSettings.Instance.PostsPerPage.ToString();
     cbShowRelatedPosts.Checked = BlogSettings.Instance.EnableRelatedPosts;
-    ddlTheme.SelectedValue = BlogSettings.Instance.Theme;    
+    ddlTheme.SelectedValue = BlogSettings.Instance.Theme;
 
-    // Comments
+    //-----------------------------------------------------------------------
+    // Bind Comments settings
+    //-----------------------------------------------------------------------
     cbEnableComments.Checked = BlogSettings.Instance.IsCommentsEnabled;
     cbEnableCountryInComments.Checked = BlogSettings.Instance.EnableCountryInComments;
     cbEnableCoComment.Checked = BlogSettings.Instance.IsCoCommentEnabled;
     cbShowLivePreview.Checked = BlogSettings.Instance.ShowLivePreview;
     ddlCloseComments.SelectedValue = BlogSettings.Instance.DaysCommentsAreEnabled.ToString();
-    
-    // Email
+
+    //-----------------------------------------------------------------------
+    // Bind Email settings
+    //-----------------------------------------------------------------------
     txtEmail.Text = BlogSettings.Instance.Email;
     txtSmtpServer.Text = BlogSettings.Instance.SmtpServer;
     txtSmtpServerPort.Text = BlogSettings.Instance.SmtpServerPort.ToString();
@@ -107,12 +148,27 @@ public partial class admin_Pages_configuration : System.Web.UI.Page
     txtSmtpPassword.Text = BlogSettings.Instance.SmtpPassword;
     cbComments.Checked = BlogSettings.Instance.SendMailOnComment;
 
-    // Advanced
+    //-----------------------------------------------------------------------
+    // Bind Advanced settings
+    //-----------------------------------------------------------------------
     cbEnableCompression.Checked = BlogSettings.Instance.EnableHttpCompression;
     cbEnableSearchHighlight.Checked = BlogSettings.Instance.EnableSearchHightlight;
     cbRemoveWhitespaceInStyleSheets.Checked = BlogSettings.Instance.RemoveWhitespaceInStyleSheets;
     cbEnableOpenSearch.Checked = BlogSettings.Instance.EnableOpenSearch;
     cbMarkExternalLinks.Checked = BlogSettings.Instance.MarkExternalLinks;
+
+    //-----------------------------------------------------------------------
+    // Bind Syndication settings
+    //-----------------------------------------------------------------------
+    ddlSyndicationFormat.SelectedValue  = BlogSettings.Instance.SyndicationFormat;
+
+    txtDublinCoreCreator.Text           = BlogSettings.Instance.AuthorName;
+    txtDublinCoreLanguage.Text          = BlogSettings.Instance.Language;
+
+    txtGeocodingLatitude.Text           = BlogSettings.Instance.GeocodingLatitude != Single.MinValue ? BlogSettings.Instance.GeocodingLatitude.ToString() : String.Empty;
+    txtGeocodingLongitude.Text          = BlogSettings.Instance.GeocodingLongitude != Single.MinValue ? BlogSettings.Instance.GeocodingLongitude.ToString() : String.Empty;
+
+    txtBlogChannelBLink.Text            = BlogSettings.Instance.Endorsement;
   }
 
   private void BindThemes()
