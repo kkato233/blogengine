d253502887f83d513488fc64bafee763701b3d09
RazorAnt <RazorAnt@localhost>
2007-05-19 05:10:55 +0000

    Minor update to MSSQL Provider

--- BlogEngine/DotNetSlave.BusinessLogic/Providers/MSSQLBlogProvider.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Providers/MSSQLBlogProvider.cs
@@ -13,7 +13,7 @@ namespace BlogEngine.Core.Providers
     /// </summary>
     public class MSSQLBlogProvider : BlogProvider
     {
-        private SqlConnection providerConn;
+        private SqlConnection providerConn; 
 
         #region Posts
         /// <summary>
@@ -21,7 +21,7 @@ namespace BlogEngine.Core.Providers
         /// </summary>
         public override Post SelectPost(Guid id)
         {
-            OpenConnection();
+            bool connClose = OpenConnection();
 
             Post post = new Post();
             string sqlQuery =   "SELECT PostID, Title, Description, PostContent, DateCreated, " +
@@ -60,7 +60,8 @@ namespace BlogEngine.Core.Providers
 
             while (rdr.Read())
             {
-                post.Tags.Add(rdr.GetString(0));
+                if (!rdr.IsDBNull(0))
+                    post.Tags.Add(rdr.GetString(0));
             }
 
             rdr.Close();
@@ -115,6 +116,9 @@ namespace BlogEngine.Core.Providers
 
             rdr.Close();
 
+            if (connClose)
+                providerConn.Close();
+
             return post;
         }
 
@@ -126,21 +130,41 @@ namespace BlogEngine.Core.Providers
             OpenConnection();
 
             string sqlQuery =   "INSERT INTO " +
-                                "be_Posts (PostID)" +
-                                "VALUES (@id)";
+                                "be_Posts (PostID, Title, Description, PostContent, DateCreated, " +
+                                "DateModified, Author, IsPublished, IsCommentEnabled)" +
+                                "VALUES (@id, @title, @desc, @content, @created, @modified, " +
+                                "@author, @published, @commentEnabled)";
             SqlCommand cmd = new SqlCommand(sqlQuery, providerConn);
             cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
+            cmd.Parameters.Add(new SqlParameter("@title", post.Title));
+            if (post.Description == null)
+                cmd.Parameters.Add(new SqlParameter("@desc", ""));
+            else
+                cmd.Parameters.Add(new SqlParameter("@desc", post.Description));
+            cmd.Parameters.Add(new SqlParameter("@content", post.Content));
+            cmd.Parameters.Add(new SqlParameter("@created", new SqlDateTime(post.DateCreated)));
+            if (post.DateModified == new DateTime())
+                cmd.Parameters.Add(new SqlParameter("@modified", new SqlDateTime()));
+            else
+                cmd.Parameters.Add(new SqlParameter("@modified", new SqlDateTime(post.DateModified)));
+            if (post.Author == null)
+                cmd.Parameters.Add(new SqlParameter("@author", ""));
+            else
+                cmd.Parameters.Add(new SqlParameter("@author", post.Author));
+            cmd.Parameters.Add(new SqlParameter("@published", post.IsPublished));
+            cmd.Parameters.Add(new SqlParameter("@commentEnabled", post.IsCommentsEnabled));
+            cmd.ExecuteNonQuery();
 
-            try
-            {
-                cmd.ExecuteNonQuery();
-            }
-            catch
-            {
-                // Do nothing.  
-            }
+            // Tags
+            UpdateTags(post);
 
-            UpdatePost(post);
+            // Categories
+            UpdateCategories(post);
+
+            // Comments
+            UpdateComments(post);
+
+            providerConn.Close();
         }
 
         /// <summary>
@@ -156,12 +180,21 @@ namespace BlogEngine.Core.Providers
                                 "IsPublished = @published, IsCommentEnabled = @commentEnabled " +
                                 "WHERE PostID = @id";
             SqlCommand cmd = new SqlCommand(sqlQuery, providerConn);
-            cmd.Parameters.Add(new SqlParameter("@title", post.Title));
-            cmd.Parameters.Add(new SqlParameter("@desc", post.Description));
+           cmd.Parameters.Add(new SqlParameter("@title", post.Title));
+            if (post.Description == null)
+                cmd.Parameters.Add(new SqlParameter("@desc", ""));
+            else
+                cmd.Parameters.Add(new SqlParameter("@desc", post.Description));
             cmd.Parameters.Add(new SqlParameter("@content", post.Content));
             cmd.Parameters.Add(new SqlParameter("@created", new SqlDateTime(post.DateCreated)));
-            cmd.Parameters.Add(new SqlParameter("@modified", new SqlDateTime(post.DateCreated)));
-            cmd.Parameters.Add(new SqlParameter("@author", post.Author));
+            if (post.DateModified == new DateTime())
+                cmd.Parameters.Add(new SqlParameter("@modified", new SqlDateTime()));
+            else
+                cmd.Parameters.Add(new SqlParameter("@modified", new SqlDateTime(post.DateModified)));
+            if (post.Author == null)
+                cmd.Parameters.Add(new SqlParameter("@author", ""));
+            else
+                cmd.Parameters.Add(new SqlParameter("@author", post.Author));
             cmd.Parameters.Add(new SqlParameter("@published", post.IsPublished));
             cmd.Parameters.Add(new SqlParameter("@commentEnabled", post.IsCommentsEnabled));
             cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
@@ -169,72 +202,15 @@ namespace BlogEngine.Core.Providers
             cmd.ExecuteNonQuery();
 
             // Tags
-            cmd.CommandText = "DELETE FROM be_PostTag WHERE PostID = @id";
-            cmd.Parameters.Clear();
-            cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
-            cmd.ExecuteNonQuery();
-
-            foreach (string tag in post.Tags)
-            {
-                cmd.CommandText = "INSERT INTO be_PostTag (PostID, Tag) VALUES (@id, @tag)";
-                cmd.Parameters.Clear();
-                cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
-                cmd.Parameters.Add(new SqlParameter("@tag", tag));
-                cmd.ExecuteNonQuery();
-            }
+            UpdateTags(post);
 
             // Categories
-            cmd.CommandText = "DELETE FROM be_PostCategory WHERE PostID = @id";
-            cmd.Parameters.Clear();
-            cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
-            cmd.ExecuteNonQuery();
-
-            foreach (Guid key in post.Categories)
-            {
-                if (CategoryDictionary.Instance.ContainsKey(key))
-                {
-                    cmd.CommandText = "INSERT INTO be_PostCategory (PostID, CategoryID) VALUES (@id, @cat)";
-                    cmd.Parameters.Clear();
-                    cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
-                    cmd.Parameters.Add(new SqlParameter("@cat", key.ToString()));
-                    cmd.ExecuteNonQuery();
-                }
-            }
+            UpdateCategories(post);
 
             // Comments
-            cmd.CommandText = "DELETE FROM be_PostComment WHERE PostID = @id";
-            cmd.Parameters.Clear();
-            cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
-            cmd.ExecuteNonQuery();
+            UpdateComments(post);
 
-            foreach (Comment comment in post.Comments)
-            {
-                sqlQuery =  "INSERT INTO be_PostComment (PostID, CommentDate, Author, Email, Website, Comment, Country, Ip) " +
-                            "VALUES (@id, @date, @author, @email, @website, @comment, @country, @ip)";
-                cmd.CommandText = sqlQuery;
-                cmd.Parameters.Clear();
-                cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
-                cmd.Parameters.Add(new SqlParameter("@date", new SqlDateTime(comment.DateCreated)));
-                cmd.Parameters.Add(new SqlParameter("@author", comment.Author));
-                cmd.Parameters.Add(new SqlParameter("@email", comment.Email));
-                if (comment.Website == null)
-                    cmd.Parameters.Add(new SqlParameter("@website", ""));
-                else
-                    cmd.Parameters.Add(new SqlParameter("@website", comment.Website.ToString()));
-                cmd.Parameters.Add(new SqlParameter("@comment", comment.Content));
-                if (comment.Country == null)
-                    cmd.Parameters.Add(new SqlParameter("@country", ""));
-                else
-                    cmd.Parameters.Add(new SqlParameter("@country", comment.Country));
-                if (comment.IP == null)
-                    cmd.Parameters.Add(new SqlParameter("@ip", ""));
-                else
-                    cmd.Parameters.Add(new SqlParameter("@ip", comment.IP));
-                
-                cmd.ExecuteNonQuery();
-            }
-            
-
+            providerConn.Close();
 
         }
 
@@ -253,6 +229,8 @@ namespace BlogEngine.Core.Providers
             cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
             
             cmd.ExecuteNonQuery();
+
+            providerConn.Close();
         }
 
         /// <summary>
@@ -275,6 +253,8 @@ namespace BlogEngine.Core.Providers
                 posts.Add(Post.Load(new Guid(dr[0].ToString())));
             }
 
+            providerConn.Close();
+
             posts.Sort();
             return posts;
         }
@@ -286,7 +266,7 @@ namespace BlogEngine.Core.Providers
         /// </summary>
         public override Page SelectPage(Guid id)
         {
-            OpenConnection();
+            bool connClose = OpenConnection();
 
             Page page = new Page();
             string sqlQuery =   "SELECT PageID, Title, Description, PageContent, DateCreated, " +
@@ -312,6 +292,9 @@ namespace BlogEngine.Core.Providers
 
             rdr.Close();
 
+            if (connClose)
+                providerConn.Close();
+
             return page;
         }
 
@@ -322,20 +305,19 @@ namespace BlogEngine.Core.Providers
         {
             OpenConnection();
 
-            string sqlQuery =   "INSERT INTO be_Pages (PageID) VALUES (@id)";
+            string sqlQuery =   "INSERT INTO be_Pages (PageID, Title, Description, PageContent, " + 
+                                "DateCreated, DateModified, Keywords) " +
+                                "VALUES (@id, @title, @desc, @content, @created, @modified, @keywords)";
             SqlCommand cmd = new SqlCommand(sqlQuery, providerConn);
             cmd.Parameters.Add(new SqlParameter("@id", page.Id.ToString()));
-
-            try
-            {
-                cmd.ExecuteNonQuery();
-            }
-            catch
-            {
-                // Do nothing, yet.
-            }
-
-            UpdatePage(page);
+            cmd.Parameters.Add(new SqlParameter("@title", page.Title));
+            cmd.Parameters.Add(new SqlParameter("@desc", page.Description));
+            cmd.Parameters.Add(new SqlParameter("@content", page.Content));
+            cmd.Parameters.Add(new SqlParameter("@created", new SqlDateTime(page.DateCreated)));
+            cmd.Parameters.Add(new SqlParameter("@modified", new SqlDateTime(page.DateCreated)));
+            cmd.Parameters.Add(new SqlParameter("@keywords", page.Keywords));
+            cmd.ExecuteNonQuery();
+            providerConn.Close();
         }
 
         /// <summary>
@@ -359,6 +341,7 @@ namespace BlogEngine.Core.Providers
             cmd.Parameters.Add(new SqlParameter("@id", page.Id.ToString()));
 
             cmd.ExecuteNonQuery();
+            providerConn.Close();
         }
 
         /// <summary>
@@ -373,6 +356,7 @@ namespace BlogEngine.Core.Providers
             cmd.Parameters.Add(new SqlParameter("@id", page.Id.ToString()));
 
             cmd.ExecuteNonQuery();
+            providerConn.Close();
         }
 
         /// <summary>
@@ -395,6 +379,8 @@ namespace BlogEngine.Core.Providers
                 pages.Add(Page.Load(new Guid(dr[0].ToString())));
             }
 
+            providerConn.Close();
+
             return pages;
         }
         #endregion
@@ -407,22 +393,21 @@ namespace BlogEngine.Core.Providers
         {
             CategoryDictionary dic = new CategoryDictionary();
 
-            OpenConnection();
+            SqlConnection conn = new SqlConnection(BlogSettings.Instance.MSSQLConnectionString);
 
-            string sqlQuery =   "SELECT CategoryID, CategoryName " +
-                                "FROM be_Categories ";
-            SqlCommand cmd = new SqlCommand(sqlQuery, providerConn);
-            SqlDataReader rdr = cmd.ExecuteReader();
+            string sqlQuery =   "SELECT CategoryID, CategoryName FROM be_Categories ";
+            SqlDataAdapter sa = new SqlDataAdapter(sqlQuery, conn);
+            DataTable dtCategories = new DataTable();
+            sa.Fill(dtCategories);
 
-            while (rdr.Read())
+            foreach (DataRow dr in dtCategories.Rows)
             {
-                Guid id = rdr.GetGuid(0);
-                string title = rdr.GetString(1);
+                Guid id = new Guid(dr[0].ToString());
+                string title = dr[1].ToString();
                 dic.Add(id, title);
             }
 
-            rdr.Close();
-
+            conn.Close();
             return dic;
         }
 
@@ -431,10 +416,10 @@ namespace BlogEngine.Core.Providers
         /// </summary>
         public override void SaveCategories(CategoryDictionary categories)
         {
-            OpenConnection();
+            SqlConnection conn = new SqlConnection(BlogSettings.Instance.MSSQLConnectionString);
 
             string sqlQuery = "DELETE FROM be_Categories";
-            SqlCommand cmd = new SqlCommand(sqlQuery, providerConn);
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
             cmd.ExecuteNonQuery();
 
             foreach (Guid key in categories.Keys)
@@ -447,20 +432,107 @@ namespace BlogEngine.Core.Providers
                 cmd.Parameters.Add(new SqlParameter("@id", categories[key]));
                 cmd.ExecuteNonQuery();
             }
+
+            conn.Close();
+
         }
         #endregion
 
         /// <summary>
         /// Handles Opening the SQL Connection
         /// </summary>
-        private void OpenConnection()
+        private bool OpenConnection()
         {
+            bool result = false;
+
             // Initial if needed
             if (providerConn == null)
                 providerConn = new SqlConnection(BlogSettings.Instance.MSSQLConnectionString);
             // Open it if needed
             if (providerConn.State == System.Data.ConnectionState.Closed)
+            {
+                result = true;
                 providerConn.Open();
+            }
+
+            return result;
+        }
+
+        private void UpdateTags(Post post)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = providerConn;
+            cmd.CommandText = "DELETE FROM be_PostTag WHERE PostID = @id";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
+            cmd.ExecuteNonQuery();
+
+            foreach (string tag in post.Tags)
+            {
+                cmd.CommandText = "INSERT INTO be_PostTag (PostID, Tag) VALUES (@id, @tag)";
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
+                cmd.Parameters.Add(new SqlParameter("@tag", tag));
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void UpdateCategories(Post post)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = providerConn;
+            cmd.CommandText = "DELETE FROM be_PostCategory WHERE PostID = @id";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
+            cmd.ExecuteNonQuery();
+
+            foreach (Guid key in post.Categories)
+            {
+                if (CategoryDictionary.Instance.ContainsKey(key))
+                {
+                    cmd.CommandText = "INSERT INTO be_PostCategory (PostID, CategoryID) VALUES (@id, @cat)";
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@cat", key.ToString()));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void UpdateComments(Post post)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = providerConn;
+            cmd.CommandText = "DELETE FROM be_PostComment WHERE PostID = @id";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
+            cmd.ExecuteNonQuery();
+
+            foreach (Comment comment in post.Comments)
+            {
+                cmd.CommandText =   "INSERT INTO be_PostComment (PostID, CommentDate, Author, Email, Website, Comment, Country, Ip) " +
+                                    "VALUES (@id, @date, @author, @email, @website, @comment, @country, @ip)";
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add(new SqlParameter("@id", post.Id.ToString()));
+                cmd.Parameters.Add(new SqlParameter("@date", new SqlDateTime(comment.DateCreated)));
+                cmd.Parameters.Add(new SqlParameter("@author", comment.Author));
+                cmd.Parameters.Add(new SqlParameter("@email", comment.Email));
+                if (comment.Website == null)
+                    cmd.Parameters.Add(new SqlParameter("@website", ""));
+                else
+                    cmd.Parameters.Add(new SqlParameter("@website", comment.Website.ToString()));
+                cmd.Parameters.Add(new SqlParameter("@comment", comment.Content));
+                if (comment.Country == null)
+                    cmd.Parameters.Add(new SqlParameter("@country", ""));
+                else
+                    cmd.Parameters.Add(new SqlParameter("@country", comment.Country));
+                if (comment.IP == null)
+                    cmd.Parameters.Add(new SqlParameter("@ip", ""));
+                else
+                    cmd.Parameters.Add(new SqlParameter("@ip", comment.IP));
+
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
