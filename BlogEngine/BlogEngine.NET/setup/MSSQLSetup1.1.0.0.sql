c3589ca0131699a8bff618d9733086bef0c9d3ed
RazorAnt <RazorAnt@localhost>
2007-07-13 20:49:03 +0000

    Updated MSSQL Setup Script, Theme Updates

--- BlogEngine/BlogEngine.NET/setup/MSSQLSetup1.1.0.0.sql
+++ BlogEngine/BlogEngine.NET/setup/MSSQLSetup1.1.0.0.sql
@@ -167,3 +167,130 @@ CREATE TABLE [dbo].[be_Settings](
 
 GO
 SET ANSI_PADDING OFF
+
+/***  Load initial Data ***/
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('storagelocation', '~/app_data/');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('enablecountryincomments', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('searchcommentlabeltext', 'Include comments in search');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('enablerelatedposts', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('removewhitespaceinstylesheets', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('endorsement', 'http://www.dotnetblogengine.net/syndication.axd?format=rss');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('showlivepreview', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('enablesearchhightlight', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('feedburnerusername', '');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('authorname', '');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('displaycommentsonrecentposts', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('theme', 'standard');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('iscommentsenabled', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('language', 'en-GB');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('postsperpage', '10');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('htmlheader', '<link rel="example" />');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('syndicationformat', 'Rss');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('geocodinglatitude', '0');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('email', 'user@example.com');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('dayscommentsareenabled', '0');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('enablecommentsearch', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('blogrollvisibleposts', '3');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('timezone', '1');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('smtpusername', 'user@example.com');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('searchdefaulttext', 'Enter search term');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('iscocommentenabled', 'False');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('trackingscript', '');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('description', 'Short description of the blog');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('enableopensearch', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('numberofrecentposts', '10');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('displayratingsonrecentposts', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('searchbuttontext', 'Search');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('name', 'Name of the blog');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('culture', 'Auto');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('contactformmessage', '<p>I will answer the mail as soon as I can.</p>');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('smtppassword', 'password');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('smtpserverport', '25');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('smtpserver', 'mail.example.dk');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('contactthankmessage', '<h1>Thank you</h1><p>The message was sent.</p>');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('postsperfeed', '15');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('geocodinglongitude', '0');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('enablereferrertracking', 'False');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('blogrollupdateminutes', '60');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('sendmailoncomment', 'True');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('blogrollmaxlength', '30');
+INSERT INTO be_Settings (SettingName, SettingValue)
+	VALUES ('enablehttpcompression', 'True');
+
+DECLARE @postID uniqueidentifier, @catID uniqueidentifier
+
+SET @postID = NEWID();
+SET @catID = NEWID();
+
+INSERT INTO be_Categories (CategoryID, CategoryName)
+	VALUES (@catID, 'General');
+
+INSERT INTO be_Posts (PostID, Title, Description, PostContent, DateCreated, Author, IsPublished)
+	VALUES (@postID, 
+	'Welcome to BlogEngine.NET', 
+	'The description is used as the meta description as well as shown in the related posts. It is recommended that you write a description, but not mandatory',
+	'<p>If you see this post it means that BlogEngine.NET is running and the hard part of creating your own blog is done. There is only one thing you need to do from this point on to take full advantage of the blog and that is to set up the first author profile.</p>
+	<h2>Setup</h2>
+	<p>Find and open the users.xml file which is located in the App_Data folder. Edit the default user and provide your own name as the username and a password of your choice. Save the users.xml file with the new username and password and you are now able to log in and start writing posts.</p>
+	<h2>Write permissions</h2>
+	<p>To be able to log in to the blog and writing posts, you need to enable write permissions on the App_Data folder. If you&amp;rsquo;re blog is hosted at a hosting provider, you can either log into your account&amp;rsquo;s admin page or call the support. You need write permissions on the App_Data folder because all posts and comments are saved as XML files and placed in the App_Data folder.</p>
+	<h2>On the web </h2>
+	<p>You can find BlogEngine.NET on the &lt;a href="http://www.dotnetblogengine.net"&gt;official website&lt;/a&gt;. Here you&amp;#39;ll find tutorials, documentation, tips and tricks and much more. The ongoing development of BlogEngine.NET can be followed at &lt;a href="http://www.codeplex.com/blogengine"&gt;CodePlex&lt;/a&gt; where the daily builds will be published for anyone to download.</p>
+	<p>Good luck and happy writing.</p>
+	<p>The BlogEngine.NET team</p>',
+	'05/06/07', 
+	'admin',
+	1);
+
+INSERT INTO be_PostCategory (PostID, CategoryID)
+	VALUES (@postID, @catID);
+
+INSERT INTO be_PostTag (PostID, Tag)
+	VALUES (@postID, 'blog');
+INSERT INTO be_PostTag (PostID, Tag)
+	VALUES (@postID, 'welcome');
--- BlogEngine/BlogEngine.NET/themes/dirtylicious/PostView.ascx
+++ BlogEngine/BlogEngine.NET/themes/dirtylicious/PostView.ascx
@@ -3,9 +3,12 @@
 <div class="post xfolkentry">
     <h1><a class="postheader taggedlink" href="<%=Post.RelativeLink %>"><%=Post.Title %></a></h1>
     <div class="descr"><%=Post.DateCreated.ToString("MMMM d, yyyy HH:mm")%> by <a href="<%=VirtualPathUtility.ToAbsolute("~/") + "author/" + Post.Author %>.aspx"><%=Post.Author %></a></div>
-    <div class="entry"><%=Post.Content %></div>
+    <div class="entry"><%=Post.Content %>
+    
+        <br /><%=Rating %>
+    </div>
     <div class="info">
-        Tags: <%=TagLinks(", ") %><br />
+        <%=TagLinks(", ") %><br />
         Categories: <%=CategoryLinks(" | ") %><br />
         Actions: <%=AdminLinks %>
         <a rel="nofollow" href="mailto:?subject=<%=Post.Title %>&amp;body=Thought you might like this: <%=Post.AbsoluteLink.ToString() %>">E-mail</a> | 
