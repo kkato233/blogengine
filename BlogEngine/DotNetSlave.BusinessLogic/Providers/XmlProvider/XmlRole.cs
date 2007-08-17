54897600f7f126125695fa53abcd81c1cb0a5a40
SND\rclarkson_cp <SND\rclarkson_cp@localhost>
2007-08-17 03:37:56 +0000

    Here is the initial work up of the xmlroleprovider.  It is a little complicated right now and needs to be cleaned up.

---
+++ BlogEngine/DotNetSlave.BusinessLogic/Providers/XmlProvider/XmlRole.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogEngine.Core.Providers
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class XmlRole {
+
+        #region Fields  
+
+        public string Name = string.Empty;
+        public List<string> Users = new List<string>(); 
+
+        #endregion
+    }
+}
