3579ac9a5876a984394dfab2a8b904cfa0f3e81e
SND\rclarkson_cp <SND\rclarkson_cp@localhost>
2007-08-25 00:15:28 +0000

    Here is a working version of the Syntax Highlighter, Code Formatter, or whatever you want to call it.  I worked up an extension with some code in the App_Code directory.  Enjoy.

--- BlogEngine/BlogEngine.NET/App_Code/Extensions/CodeFormatters.cs
+++ BlogEngine/BlogEngine.NET/App_Code/Extensions/CodeFormatters.cs
@@ -20,6 +20,17 @@ using CodeFormatter;
 [Extension("Changes <code:lang></code>.  Adapted from Jean-Claude Manoli [jc@manoli.net].", "0.0.0.1", "www.manoli.net")]
 public class CodeFormatters
 {
+
+    //Characters other than . $ ^ { [ ( | ) * + ? \ match themselves.
+    //Pretty close with this  <code[^>]*>[\w|\t|\r|\W]*</code>
+    static readonly Regex _CSharpRegex = new Regex(@"\[code\:c\#].*\[code]", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.Singleline);
+    static readonly Regex _VBRegex = new Regex(@"\[code\:vb].*\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+    static readonly Regex _JavaScriptRegex = new Regex(@"\[code\:js].*\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+    static readonly Regex _HTMLRegex = new Regex(@"\[code\:html].*\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+    static readonly Regex _XMLRegex = new Regex(@"\[code\:xml].*\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+    static readonly Regex _TSQLRegex = new Regex(@"\[code\:tsql].*\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+    static readonly Regex _MSHRegex = new Regex(@"\[code\:msh].*\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+
     private enum CodeLanguages
     {
         csharp, vb, js, html, xml, tsql, msh
@@ -38,21 +49,11 @@ public class CodeFormatters
          * JS
          * HTML
          * XML
-         * T-SQL
+         * TSQL
          * MSH (code name Monad) --I don't have a clue.
          */
         if (!string.IsNullOrEmpty(e.Body))
         {
-
-            //Characters other than . $ ^ { [ ( | ) * + ? \ match themselves.
-            Regex _CSharpRegex = new Regex(@"\[code:c#](.*?)\[code]", RegexOptions.IgnoreCase |  RegexOptions.Compiled);
-            Regex _VBRegex = new Regex(@"\[code:vb](.*?)\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
-            Regex _JavaScriptRegex = new Regex(@"\[code:js](.*?)\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
-            Regex _HTMLRegex = new Regex(@"\[code:html](.*?)\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
-            Regex _XMLRegex = new Regex(@"\[code:xml](.*?)\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
-            Regex _TSQLRegex = new Regex(@"\[code:tsql](.*?)\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
-            Regex _MSHRegex = new Regex(@"\[code:msh](.*?)\[code]", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
-
             e.Body = _CSharpRegex.Replace(e.Body, new MatchEvaluator(MatchEval));
             e.Body = _VBRegex.Replace(e.Body, new MatchEvaluator(MatchEval));
             e.Body = _JavaScriptRegex.Replace(e.Body, new MatchEvaluator(MatchEval));
@@ -66,49 +67,54 @@ public class CodeFormatters
 
     private static string MatchEval(Match match)
     {
-        if (match.Groups[1].Success)
+        foreach (Match mat in match.Groups)
         {
-            string matchString = match.ToString();
-            string langType = matchString.Remove(0, 6);
-            langType = langType.Remove(langType.IndexOf("]"));
-            matchString = matchString.Replace("[code]", "");
-
-
-            switch (langType)
+            if (mat.Success)
             {
-                case "c#":
-                    matchString = matchString.Replace("[code:c#]", "");
-                    CSharpFormat csf = new CSharpFormat();
-                    return csf.FormatCode(matchString);
-                case "vb":
-                    matchString = matchString.Replace("[code:vb]", "");
-                    CodeFormatter.VisualBasicFormat vbf = new VisualBasicFormat();
-                    return vbf.FormatCode(matchString);
-                case "js":
-                    matchString = matchString.Replace("[code:js]", "");
-                    CodeFormatter.JavaScriptFormat jsf = new JavaScriptFormat();
-                    return jsf.FormatCode(matchString);
-                case "html":
-                    matchString = matchString.Replace("[code:html]", "");
-                    CodeFormatter.HtmlFormat htmlf = new HtmlFormat();
-                    return htmlf.FormatCode(matchString);
-                case "xml":
-                    matchString = matchString.Replace("[code:xml]", "");
-                    CodeFormatter.HtmlFormat xmlf = new HtmlFormat();
-                    return xmlf.FormatCode(matchString);
-                case "tsql":
-                    matchString = matchString.Replace("[code:tsql]", "");
-                    CodeFormatter.TsqlFormat tsqlf = new TsqlFormat();
-                    return tsqlf.FormatCode(matchString);
-                case "msh":
-                    matchString = matchString.Replace("[code:msh]", "");
-                    CodeFormatter.MshFormat mshf = new MshFormat();
-                    return mshf.FormatCode(matchString);
+                string matchString = match.ToString();
+                string langType = matchString.Remove(0, 6);
+                langType = langType.Remove(langType.IndexOf("]"));
+
+                switch (langType)
+                {
+                    case "c#":
+                        matchString = matchString.Replace("[code:c#]", "");
+                        matchString = matchString.Replace("[code]", "");
+                        CSharpFormat csf = new CSharpFormat();
+                        return HttpContext.Current.Server.HtmlDecode(csf.FormatCode(matchString));
+                    case "vb":
+                        matchString = matchString.Replace("[code:vb]", "");
+                        matchString = matchString.Replace("[code]", "");
+                        CodeFormatter.VisualBasicFormat vbf = new VisualBasicFormat();
+                        return HttpContext.Current.Server.HtmlEncode(vbf.FormatCode(matchString));
+                    case "js":
+                        matchString = matchString.Replace("[code:js]", "");
+                        matchString = matchString.Replace("[code]", "");
+                        CodeFormatter.JavaScriptFormat jsf = new JavaScriptFormat();
+                        return jsf.FormatCode(matchString);
+                    case "html":
+                        matchString = matchString.Replace("[code:html]", "");
+                        matchString = matchString.Replace("[code]", "");
+                        CodeFormatter.HtmlFormat htmlf = new HtmlFormat();
+                        return htmlf.FormatCode(matchString);
+                    case "xml":
+                        matchString = matchString.Replace("[code:xml]", "");
+                        matchString = matchString.Replace("[code]", "");
+                        CodeFormatter.HtmlFormat xmlf = new HtmlFormat();
+                        return xmlf.FormatCode(matchString);
+                    case "tsql":
+                        matchString = matchString.Replace("[code:tsql]", "");
+                        matchString = matchString.Replace("[code]", "");
+                        CodeFormatter.TsqlFormat tsqlf = new TsqlFormat();
+                        return tsqlf.FormatCode(matchString);
+                    case "msh":
+                        matchString = matchString.Replace("[code:msh]", "");
+                        matchString = matchString.Replace("[code]", "");
+                        CodeFormatter.MshFormat mshf = new MshFormat();
+                        return mshf.FormatCode(matchString);
+                }
             }
         }
         return ""; //no match
     }
-
-
-
 }
