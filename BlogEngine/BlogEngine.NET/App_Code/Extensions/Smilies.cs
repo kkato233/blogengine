a3ed83523077dce74ee3c439a1be65c1db0cb8e6
madskristensen <madskristensen@localhost>
2007-09-13 22:34:54 +0000

    Minor fixes and niceties

--- BlogEngine/BlogEngine.NET/App_Code/Extensions/Smilies.cs
+++ BlogEngine/BlogEngine.NET/App_Code/Extensions/Smilies.cs
@@ -1,92 +1,57 @@
 #region using
 
 using System;
-using System.IO;
-using System.Web;
-using System.Text.RegularExpressions;
 using BlogEngine.Core;
 using BlogEngine.Core.Web.Controls;
 
 #endregion
 
-/*
-Plugin Name: Smilies
-Plugin URI: http://cake.happytocode.com
-Description: Changes smilies from text to image inside a comment post.
-Version: 0.0.0.1
-Author: John Knipper
-Author URI: http://www.happytocode.com
-*/
-
 /// <summary>
-/// Changes smilies from text to image inside a comment post.
+/// Converts ASCII smilies into real smilies in the comments
 /// </summary>
-[Extension("Changes smilies from text to image inside a comment post.", "0.0.0.1", "www.happytocode.com")]
+[Extension("Converts ASCII smilies into real smilies in the comments", "1.0.0.0", "BlogEngine.NET")]
 public class Smilies
 {
+
   public Smilies()
   {
     Comment.Serving += new EventHandler<ServingEventArgs>(Post_CommentServing);
   }
 
-  void Post_CommentServing(object sender, ServingEventArgs e)
-  {
-    /*  It does support only the few smilies included by default in the FCKEditor.
-        cool  (H)
-        cry :'(
-        embarassed  :$
-        foot-in-mouth :|
-        frown :(
-        innocent  (A)
-        kiss  (K)
-        laughing  :D
-        money-mouth ($)
-        sealed  :-#
-        smile :)
-        surprised :-O
-        tongue-out  :P
-        undecided *-)
-        wink ;)
-        yell  8o|
-     */
+  private static readonly string _Link = "<img src=\"{0}admin/tiny_mce/plugins/emotions/images/smiley-{1}.gif\" class=\"flag\" alt=\"{2}\" />";
+
+  /// <summary>
+  /// The event handler that is triggered every time a comment is served to a client.
+  /// </summary>
+  private void Post_CommentServing(object sender, ServingEventArgs e)
+  {    
     if (!string.IsNullOrEmpty(e.Body))
     {
-      //Characters other than . $ ^ { [ ( | ) * + ? \ match themselves.
-      e.Body = Regex.Replace(e.Body, @"\(H\)", Smiley("cool", "Cool"));
-      e.Body = Regex.Replace(e.Body, @":'\(", Smiley("cry", "Cry"));
-      e.Body = Regex.Replace(e.Body, @":\$", Smiley("embarassed", "Embarassed"));
-      e.Body = Regex.Replace(e.Body, @":\|", Smiley("foot-in-mouth", "Foot"));
-      e.Body = Regex.Replace(e.Body, @":\(", Smiley("frown", "Frown"));
-      e.Body = Regex.Replace(e.Body, @"\(A\)", Smiley("innocent", "Innocent"));
-      e.Body = Regex.Replace(e.Body, @"\(K\)", Smiley("kiss", "Kiss"));
-      e.Body = Regex.Replace(e.Body, @":D", Smiley("laughing", "Laughing"));
-      e.Body = Regex.Replace(e.Body, @"\(\$\)", Smiley("money-mouth", "Money"));
-      e.Body = Regex.Replace(e.Body, @":-#", Smiley("sealed", "Sealed"));
-      e.Body = Regex.Replace(e.Body, @":\)", Smiley("smile", "Smile"));
-      e.Body = Regex.Replace(e.Body, @":-O", Smiley("surprised", "Surprised"));
-      e.Body = Regex.Replace(e.Body, @":P", Smiley("tongue-out", "Tong"));
-      e.Body = Regex.Replace(e.Body, @"\*-\)", Smiley("undecided", "Undecided"));
-      e.Body = Regex.Replace(e.Body, @";\)", Smiley("wink", "Wink"));
-      e.Body = Regex.Replace(e.Body, @"8o\|", Smiley("yell", "Yell"));
+      e.Body = e.Body.Replace("(H)", Convert("cool", "Cool"));
+      e.Body = e.Body.Replace(":'(", Convert("cry", "Cry"));
+      e.Body = e.Body.Replace(":$", Convert("embarassed", "Embarassed"));
+      e.Body = e.Body.Replace(":|", Convert("foot-in-mouth", "Foot"));
+      e.Body = e.Body.Replace(":(", Convert("frown", "Frown"));
+      e.Body = e.Body.Replace("(A)", Convert("innocent", "Innocent"));
+      e.Body = e.Body.Replace("(K)", Convert("kiss", "Kiss"));
+      e.Body = e.Body.Replace(":D", Convert("laughing", "Laughing"));
+      e.Body = e.Body.Replace("($)", Convert("money-mouth", "Money"));
+      e.Body = e.Body.Replace(":-#", Convert("sealed", "Sealed"));
+      e.Body = e.Body.Replace(":)", Convert("smile", "Smile"));
+      e.Body = e.Body.Replace(":-)", Convert("smile", "Smile"));
+      e.Body = e.Body.Replace(":-O", Convert("surprised", "Surprised"));
+      e.Body = e.Body.Replace(":P", Convert("tongue-out", "Tong"));
+      e.Body = e.Body.Replace("*-)", Convert("undecided", "Undecided"));
+      e.Body = e.Body.Replace(";)", Convert("wink", "Wink"));
+      e.Body = e.Body.Replace("8o|", Convert("yell", "Yell"));
     }
   }
 
   /// <summary>
-  /// Does translate a smiley file name to the corresponding HTML.
+  /// Formats the anchor and inserts the right smiley image.
   /// </summary>
-  string Smiley(string name, string alt)
+  private static string Convert(string name, string alt)
   {
-    string smiley = string.Empty;
-
-    if (!string.IsNullOrEmpty(name))
-    {
-      string path =
-          HttpContext.Current.Server.MapPath(string.Format("~/admin/tiny_mce/plugins/emotions/images/smiley-{0}.gif", name));
-      if (File.Exists(path))
-      {
-        smiley = string.Format("<img src=\"{0}admin/tiny_mce/plugins/emotions/images/smiley-{1}.gif\" class=\"flag\" alt=\"{2}\" />", Utils.RelativeWebRoot, name, alt);
-      }
-    }
-    return smiley;
+    return string.Format(_Link, Utils.RelativeWebRoot, name, alt);
   }
 }
