e10ddc4f630fb9cc96d5d680adba6d47aaabff75
rusvdw <rusvdw@localhost>
2007-11-28 20:37:34 +0000

    Added a fix for postback errors using URL rewriting on Mono.

--- BlogEngine/BlogEngine.NET/post.aspx.cs
+++ BlogEngine/BlogEngine.NET/post.aspx.cs
@@ -12,87 +12,96 @@ using BlogEngine.Core.Web.Controls;
 
 public partial class post : BlogEngine.Core.Web.Controls.BlogBasePage
 {
-  protected void Page_Init(object sender, EventArgs e)
-  {
-    if (!Page.IsPostBack && !Page.IsCallback)
+    protected void Page_Init(object sender, EventArgs e)
     {
-      if (Request.RawUrl.Contains("?id="))
-      {
-        Guid id = new Guid(Request.QueryString["id"]);
-        Post post = Post.GetPost(id);
-        if (post != null)
+        if (!Page.IsPostBack && !Page.IsCallback)
         {
-          Response.Clear();
-          Response.StatusCode = 301;
-          Response.AppendHeader("location", post.RelativeLink.ToString());
-          Response.End(); 
+            if (Request.RawUrl.Contains("?id="))
+            {
+                Guid id = new Guid(Request.QueryString["id"]);
+                Post post = Post.GetPost(id);
+                if (post != null)
+                {
+                    Response.Clear();
+                    Response.StatusCode = 301;
+                    Response.AppendHeader("location", post.RelativeLink.ToString());
+                    Response.End();
+                }
+            }
         }
-      }
+
+        if (Request.QueryString["id"] != null && Request.QueryString["id"].Length == 36)
+        {
+            Guid id = new Guid(Request.QueryString["id"]);
+            this.Post = Post.GetPost(id);
+
+            if (Post != null)
+            {
+                if (!this.Post.IsVisible && !Page.User.Identity.IsAuthenticated)
+                    Response.Redirect(Utils.RelativeWebRoot + "error404.aspx", true);
+
+                string path = Utils.RelativeWebRoot + "themes/" + BlogSettings.Instance.Theme + "/PostView.ascx";
+
+                PostViewBase postView = (PostViewBase)LoadControl(path);
+                postView.Post = Post;
+                postView.Location = ServingLocation.SinglePost;
+
+                pwPost.Controls.Add(postView);
+                related.Post = this.Post;
+                CommentView1.Post = Post;
+
+                Page.Title = Server.HtmlEncode(Post.Title);
+                AddMetaKeywords();
+                AddMetaDescription();
+                AddGenericLink("last", Post.Posts[0].Title, Post.Posts[0].RelativeLink.ToString());
+                AddGenericLink("first", Post.Posts[Post.Posts.Count - 1].Title, Post.Posts[Post.Posts.Count - 1].RelativeLink.ToString());
+
+                if (Post.Next != null)
+                    base.AddGenericLink("next", Post.Next.Title, Post.Next.RelativeLink.ToString());
+
+                if (Post.Previous != null)
+                    base.AddGenericLink("prev", Post.Previous.Title, Post.Previous.RelativeLink.ToString());
+
+                Response.AppendHeader("x-pingback", "http://" + Request.Url.Authority + Utils.RelativeWebRoot + "pingback.axd");
+            }
+        }
     }
 
-    if (Request.QueryString["id"] != null && Request.QueryString["id"].Length == 36)
+    /// <summary>
+    /// Adds the post's description as the description metatag.
+    /// </summary>
+    private void AddMetaDescription()
     {
-      Guid id = new Guid(Request.QueryString["id"]);
-      this.Post = Post.GetPost(id);
-
-      if (Post != null)
-      {
-        if (!this.Post.IsVisible && !Page.User.Identity.IsAuthenticated)
-          Response.Redirect(Utils.RelativeWebRoot + "error404.aspx", true);
-
-        string path = Utils.RelativeWebRoot + "themes/" + BlogSettings.Instance.Theme + "/PostView.ascx";
-
-        PostViewBase postView = (PostViewBase)LoadControl(path);
-        postView.Post = Post;
-        postView.Location = ServingLocation.SinglePost;
-
-        pwPost.Controls.Add(postView);
-        related.Post = this.Post;
-        CommentView1.Post = Post;
-
-        Page.Title = Server.HtmlEncode(Post.Title);
-        AddMetaKeywords();
-        AddMetaDescription();
-        AddGenericLink("last", Post.Posts[0].Title, Post.Posts[0].RelativeLink.ToString());
-        AddGenericLink("first", Post.Posts[Post.Posts.Count - 1].Title, Post.Posts[Post.Posts.Count - 1].RelativeLink.ToString());
-
-        if (Post.Next != null)
-          base.AddGenericLink("next", Post.Next.Title, Post.Next.RelativeLink.ToString());
-
-        if (Post.Previous != null)
-          base.AddGenericLink("prev", Post.Previous.Title, Post.Previous.RelativeLink.ToString());
-
-        Response.AppendHeader("x-pingback", "http://" + Request.Url.Authority + Utils.RelativeWebRoot + "pingback.axd");
-      }
+        if (!string.IsNullOrEmpty(Post.Description))
+            base.AddMetaTag("description", Server.HtmlEncode(Post.Description));
+        else
+            base.AddMetaTag("description", BlogSettings.Instance.Description);
     }
-  }
 
-  /// <summary>
-  /// Adds the post's description as the description metatag.
-  /// </summary>
-  private void AddMetaDescription()
-  {
-    if (!string.IsNullOrEmpty(Post.Description))
-      base.AddMetaTag("description", Server.HtmlEncode(Post.Description));
-    else
-      base.AddMetaTag("description", BlogSettings.Instance.Description);
-  }
-
-  /// <summary>
-  /// Adds the post's tags as meta keywords.
-  /// </summary>
-  private void AddMetaKeywords()
-  {
-    if (Post.Tags.Count > 0)
+    /// <summary>
+    /// Adds the post's tags as meta keywords.
+    /// </summary>
+    private void AddMetaKeywords()
     {
-      string[] tags = new string[Post.Tags.Count];
-      for (int i = 0; i < Post.Tags.Count; i++)
-      {
-        tags[i] = Post.Tags[i];
-      }
-      base.AddMetaTag("keywords", Server.HtmlEncode(string.Join(",", tags)));
+        if (Post.Tags.Count > 0)
+        {
+            string[] tags = new string[Post.Tags.Count];
+            for (int i = 0; i < Post.Tags.Count; i++)
+            {
+                tags[i] = Post.Tags[i];
+            }
+            base.AddMetaTag("keywords", Server.HtmlEncode(string.Join(",", tags)));
+        }
     }
-  }
 
-  public Post Post;
+    public Post Post;
+
+    protected override void Render(HtmlTextWriter writer)
+    {
+        // Overwrite the default HtmlTextWriter in order to rewrite the form tag's action attribute
+        // due to mono rewrite path issues.
+        base.Render(new RewriteFormHtmlTextWriter(writer));
+    }
+
+
 }
