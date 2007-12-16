fc388ee9778101c54543d10c50968febd8b0e923
rtur <rtur@localhost>
2007-12-16 22:23:58 +0000

    Lots of changes to extension manager

--- BlogEngine/BlogEngine.NET/App_Code/Extensions/BBCode.cs
+++ BlogEngine/BlogEngine.NET/App_Code/Extensions/BBCode.cs
@@ -1,6 +1,7 @@
 #region using
 
 using System;
+using System.Data;
 using System.Text.RegularExpressions;
 using BlogEngine.Core;
 using BlogEngine.Core.Web.Controls;
@@ -10,56 +11,100 @@ using BlogEngine.Core.Web.Controls;
 /// <summary>
 /// Converts BBCode to XHTML in the comments.
 /// </summary>
-[Extension("Converts BBCode to XHTML in the comments", "1.0.0.0", "BlogEngine.NET")]
+[Extension("Converts BBCode to XHTML in the comments", "1.0.0.0", "<a href=\"http://dotnetblogengine.net\">BlogEngine.NET</a>")]
 public class BBCode
 {
+    static protected ExtensionSettings _settings = null;
 
-	public BBCode()
-	{
-		Comment.Serving += new EventHandler<ServingEventArgs>(Post_CommentServing);
-	}
+    public BBCode()
+    {
+        Comment.Serving += new EventHandler<ServingEventArgs>(Post_CommentServing);
 
-	/// <summary>
-	/// The event handler that is triggered every time a comment is served to a client.
-	/// </summary>
-	private void Post_CommentServing(object sender, ServingEventArgs e)
-	{
-		string body = e.Body;
+        // create settings object. You need to pass exactly your
+        // extension class name (case sencitive)
+        ExtensionSettings settings = new ExtensionSettings("BBCode");
 
-		Parse(ref body, "b", "strong");
-		Parse(ref body, "i", "em");
-		Parse(ref body, "u", "span style=\"text-decoration:underline\"", "span");
-		Parse(ref body, "quote", "cite title=\"Quote\"", "cite");
+        //-----------------------------------------------------
+        // 1. Simple
+        //-----------------------------------------------------
+        //settings.AddParameter("Code");
+        //settings.AddParameter("OpenTag");
+        //settings.AddParameter("CloseTag");
+        //-----------------------------------------------------
+        // 2. Some more options
+        //-----------------------------------------------------
+        //settings.AddParameter("Code");
+        //settings.AddParameter("OpenTag", "Open Tag");
+        //settings.AddParameter("CloseTag", "Close Tag");
 
-		e.Body = body;
-	}
+        //// describe specific rules applied to entering parameters. overrides default wording.
+        //settings.Help = "Converts BBCode to XHTML in the comments. Close tag is optional.";
+        //-----------------------------------------------------
+        // 3. More options including import defaults
+        //-----------------------------------------------------
+        settings.AddParameter("Code", "Code", 20, true);
+        settings.AddParameter("OpenTag", "Open Tag", 150, true);
+        settings.AddParameter("CloseTag", "Close Tag");
 
-	private static void Parse(ref string body, string code, string tag)
-	{
-		Parse(ref body, code, tag, tag);
-	}
+        // describe specific rules for entering parameters
+        settings.Help = "Converts BBCode to XHTML in the comments. Close tag is optional.";
 
-	/// <summary>
-	/// Parses the BBCode into XHTML in a safe non-breaking manor.
-	/// </summary>
-	private static void Parse(ref string body, string code, string startTag, string endTag)
-	{
-		int start = body.IndexOf("[" + code + "]");
-		if (start > -1)
-		{
-			if (body.IndexOf("[/" + code + "]", start) > -1)
-			{
-				body = body.Remove(start, code.Length + 2);
-				body = body.Insert(start, "<" + startTag + ">");
+        settings.AddValues(new string[] { "b", "strong", "" });
+        settings.AddValues(new string[] { "i", "em", "" });
+        settings.AddValues(new string[] { "u", "em,span style=\"text-decoration:underline\"", "span" });
+        settings.AddValues(new string[] { "quote", "cite title=\"Quote\"", "cite" });
+        //------------------------------------------------------
+        ExtensionManager.ImportSettings(settings);
+        _settings = ExtensionManager.GetSettings("BBCode");
+    }
 
-				int end = body.IndexOf("[/" + code + "]", start);
+    /// <summary>
+    /// The event handler that is triggered every time a comment is served to a client.
+    /// </summary>
+    private void Post_CommentServing(object sender, ServingEventArgs e)
+    {
+        string body = e.Body;
 
-				body = body.Remove(end, code.Length + 3);
-				body = body.Insert(end, "</" + endTag + ">");
+        // retrieve parameters back as a data table
+        // column = parameter
+        DataTable table = _settings.GetDataTable();
+        foreach (DataRow row in table.Rows)
+        {
+            if (string.IsNullOrEmpty((string)row["CloseTag"]))
+                Parse(ref body, (string)row["Code"], (string)row["OpenTag"]);
+            else
+                Parse(ref body, (string)row["Code"], (string)row["OpenTag"], (string)row["CloseTag"]);
+        }
 
-				Parse(ref body, code, startTag);
-			}
-		}
-	}
+        e.Body = body;
+    }
+
+    private static void Parse(ref string body, string code, string tag)
+    {
+        Parse(ref body, code, tag, tag);
+    }
+
+    /// <summary>
+    /// Parses the BBCode into XHTML in a safe non-breaking manor.
+    /// </summary>
+    private static void Parse(ref string body, string code, string startTag, string endTag)
+    {
+        int start = body.IndexOf("[" + code + "]");
+        if (start > -1)
+        {
+            if (body.IndexOf("[/" + code + "]", start) > -1)
+            {
+                body = body.Remove(start, code.Length + 2);
+                body = body.Insert(start, "<" + startTag + ">");
+
+                int end = body.IndexOf("[/" + code + "]", start);
+
+                body = body.Remove(end, code.Length + 3);
+                body = body.Insert(end, "</" + endTag + ">");
+
+                Parse(ref body, code, startTag);
+            }
+        }
+    }
 
 }
