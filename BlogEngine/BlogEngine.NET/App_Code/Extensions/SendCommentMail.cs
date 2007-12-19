6c2b535e8ad0df668db69fa0988ac68e6c14e4de
madskristensen <madskristensen@localhost>
2007-12-19 23:36:53 +0000

    Added more information to the comment mail and formatted it nicely

--- BlogEngine/BlogEngine.NET/App_Code/Extensions/SendCommentMail.cs
+++ BlogEngine/BlogEngine.NET/App_Code/Extensions/SendCommentMail.cs
@@ -37,9 +37,26 @@ public class SendCommentMail
 			mail.From = new MailAddress(from, HttpUtility.HtmlDecode(comment.Author));
 			mail.To.Add(BlogSettings.Instance.Email);
 			mail.Subject = BlogSettings.Instance.EmailSubjectPrefix + " comment on " + post.Title;
-			mail.Body = "Comment by " + comment.Author + " (" + comment.Email + ")<br /><br />";
-			mail.Body += comment.Content + "<br /><br />";
-			mail.Body += string.Format("<a href=\"{0}\">{1}</a>", post.PermaLink + "#id_" + comment.Id, post.Title);
+			mail.Body = "<div style=\"font: 11px verdana, arial\">";
+			mail.Body += "Comment by <strong>" + comment.Author + "</strong><br /><br />";
+			mail.Body += comment.Content.Replace(Environment.NewLine, "<br />") + "<br /><br />";
+			mail.Body += string.Format("<a href=\"{0}\">{1}</a><br /><br />", post.PermaLink + "#id_" + comment.Id, post.Title);
+
+			if (HttpContext.Current != null)
+			{
+				mail.Body += "_______________________________________________________________________________<br />";
+				mail.Body += "<h3>Author information</h3>";
+				mail.Body += "<div style=\"font-size:10px;line-height:16px\">";
+				mail.Body += "<strong>E-mail:</strong> " + comment.Email + "<br />";
+				mail.Body += string.Format("<strong>Website:</strong> <a href=\"{0}\">{0}</a><br />", comment.Website);
+				mail.Body += "<strong>Country code:</strong> " + comment.Country.ToUpperInvariant() + "<br />";
+				mail.Body += "<strong>IP address:</strong> " + HttpContext.Current.Request.UserHostAddress + "<br />";
+				mail.Body += string.Format("<strong>Referrer:</strong> <a href=\"{0}\">{0}</a><br />", HttpContext.Current.Request.UrlReferrer);
+				mail.Body += "<strong>User-agent:</strong> " + HttpContext.Current.Request.UserAgent;
+				mail.Body += "</div>";
+			}
+
+			mail.Body += "</div>";
 
 			Utils.SendMailMessageAsync(mail);
 		}
