945d5fa401f2001d6bda6e8a69abace056906daf
SND\rclarkson_cp <SND\rclarkson_cp@localhost>
2008-03-08 20:03:13 +0000

    Added initial profile provider support

---
+++ BlogEngine/BlogEngine.NET/admin/Pages/Profiles.aspx
@@ -0,0 +1,15 @@
+<%@ Page Language="C#" MasterPageFile="~/admin/admin1.master" AutoEventWireup="true"
+    CodeFile="Profiles.aspx.cs" Inherits="admin_profiles" Title="Modify Profiles" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphAdmin" runat="Server">
+    <br />
+    <div class="settings">
+        <h1>
+            <%=Resources.labels.userProfiles %>
+        </h1>
+        <br />
+    </div>
+    <br />
+    <div class="settings">
+    </div>
+</asp:Content>
---
+++ BlogEngine/DotNetSlave.BusinessLogic/Web/AuthorProfile.cs
@@ -0,0 +1,161 @@
+using System.Web.Profile;
+
+namespace BlogEngine.Core.Web
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AuthorProfile : ProfileBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }    
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public bool IsPrivate
+        {
+            get { return (bool) base["IsPrivate"]; }
+            set { base["IsPrivate"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string FirstName
+        {
+            get { return base["FirstName"].ToString(); }
+            set { base["FirstName"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string LastName
+        {
+            get { return base["LastName"].ToString(); }
+            set { base["LastName"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string DisplayName
+        {
+            get { return base["DisplayName"].ToString(); }
+            set { base["DisplayName"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string PhotoURL
+        {
+            get { return base["PhotoURL"].ToString(); }
+            set { base["PhotoURL"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string Gender
+        {
+            get { return base["Gender"].ToString(); }
+            set { base["Gender"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string Birthday
+        {
+            get { return base["Birthday"].ToString(); }
+            set { base["Birthday"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string CityTown
+        {
+            get { return base["CityTown"].ToString(); }
+            set { base["CityTown"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string RegionState
+        {
+            get { return base["RegionState"].ToString(); }
+            set { base["RegionState"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string Country
+        {
+            get { return base["Country"].ToString(); }
+            set { base["Country"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string Industry
+        {
+            get { return base["Industry"].ToString(); }
+            set { base["Industry"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string Occupation
+        {
+            get { return base["Occupation"].ToString(); }
+            set { base["Occupation"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string Interests
+        {
+            get { return base["Interests"].ToString(); }
+            set { base["Interests"] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SettingsAllowAnonymous(false)]
+        public string AboutMe
+        {
+            get { return base["AboutMe"].ToString(); }
+            set { base["AboutMe"] = value; }
+        }
+
+    }
+}
