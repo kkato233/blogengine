4b6dc70efcb055e04f929784b9483f48a9a8d732
SND\rclarkson_cp <SND\rclarkson_cp@localhost>
2008-03-13 17:26:50 +0000

    Added to the profile busines object in preparation for the profile provider utilization.

--- BlogEngine/DotNetSlave.BusinessLogic/Profile.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Profile.cs
@@ -9,37 +9,43 @@ namespace BlogEngine.Core
     /// <summary>
     /// This business object is to handle the profiles of users
     /// </summary>
-    [XmlRoot( "profile" )]
-    public class Profile
+    [XmlRoot("profile")]
+    public class Profile : BusinessBase<Profile, string>
     {
-        private string userName;
-        private string name;
-        private string age;
+        #region Fields
+
+        private string aboutMe;
+        private string birthDate;
+        private string cityState;
+        private string country;
+        private string displayName;
+        private string firstName;
+        private string gender;
         private string interests;
-        
+        private bool isPrivate;
+        private string lastName;
+        private string photoURL;
+        private string regionState;
+        private string userName;
+
+        #endregion
+
+        #region Properties
+
         /// <summary>
         /// 
         /// </summary>
-        [XmlElement("name")]
-        public string Name
+        [XmlElement("displayName", DataType = "string")]
+        public string DisplayName
         {
-            get { return name; }
-            set { name = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        [XmlElement("age")]
-        public string Age
-        {
-            get { return age; }
-            set { age = value; }
+            get { return displayName; }
+            set { displayName = value; }
         }
 
         /// <summary>
         /// 
         /// </summary>
-        [XmlElement("interests")]
+        [XmlElement("interests", DataType = "string")]
         public string Interests
         {
             get { return interests; }
@@ -49,12 +55,106 @@ namespace BlogEngine.Core
         /// <summary>
         /// 
         /// </summary>
-        [XmlElement("username")]
+        [XmlElement("userName", DataType = "string")]
         public string UserName
         {
             get { return userName; }
             set { userName = value; }
         }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("aboutme", DataType = "string")]
+        public string AboutMe
+        {
+            get { return aboutMe; }
+            set { aboutMe = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("firstName", DataType = "string")]
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("lastName", DataType = "string")]
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("isPrivate", DataType = "bool")]
+        public bool IsPrivate
+        {
+            get { return isPrivate; }
+            set { isPrivate = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("photoURL", DataType = "string")]
+        public string PhotoURL
+        {
+            get { return photoURL; }
+            set { photoURL = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("gender", DataType = "string")]
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("birthDate", DataType = "string")]
+        public string BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("cityState", DataType = "string")]
+        public string CityState
+        {
+            get { return cityState; }
+            set { cityState = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("regionState", DataType = "string")]
+        public string RegionState
+        {
+            get { return regionState; }
+            set { regionState = value; }
+        }
+
+        ///<summary>
+        ///</summary>
+        [XmlElement("country", DataType = "string")]
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        #endregion
+
+        #region Helpers
 
         ///<summary>
         ///</summary>
@@ -66,9 +166,11 @@ namespace BlogEngine.Core
             try
             {
                 // Deserialize the specified file to a Theater object.
-                XmlSerializer xs = new XmlSerializer(typeof(Profile));
-                FileStream fs = new FileStream(BlogSettings.Instance.StorageLocation + username.ToLowerInvariant() + ".xml", FileMode.Open);
-                profile = (Profile)xs.Deserialize(fs);
+                XmlSerializer xs = new XmlSerializer(typeof (Profile));
+                FileStream fs =
+                    new FileStream(BlogSettings.Instance.StorageLocation + username.ToLowerInvariant() + ".xml",
+                                   FileMode.Open);
+                profile = (Profile) xs.Deserialize(fs);
             }
             catch (Exception x)
             {
@@ -86,8 +188,11 @@ namespace BlogEngine.Core
             try
             {
                 // Serialize the Profile object to an XML file.
-                XmlSerializer xs = new XmlSerializer(typeof(Profile));
-                FileStream fs = new FileStream(BlogSettings.Instance.StorageLocation + userProfile.userName.ToLowerInvariant() + ".xml", FileMode.Create);
+                XmlSerializer xs = new XmlSerializer(typeof (Profile));
+                FileStream fs =
+                    new FileStream(
+                        BlogSettings.Instance.StorageLocation + userProfile.userName.ToLowerInvariant() + ".xml",
+                        FileMode.Create);
                 xs.Serialize(fs, userProfile);
             }
             catch (Exception x)
@@ -100,7 +205,7 @@ namespace BlogEngine.Core
         /// 
         /// </summary>
         /// <returns></returns>
-        public static List<Profile>  GetProfiles()
+        public static List<Profile> GetProfiles()
         {
             List<Profile> profiles = new List<Profile>();
             foreach (MembershipUser user in Membership.GetAllUsers())
@@ -111,8 +216,72 @@ namespace BlogEngine.Core
             return profiles;
         }
 
+        #endregion
 
+        #region businessBase
+
+        /// <summary>
+        /// Reinforces the business rules by adding additional rules to the 
+        /// broken rules collection.
+        /// </summary>
+        /// 
+        /// 
+        protected override void ValidationRules()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retrieves the object from the data store and populates it.
+        /// </summary>
+        /// <param name="id">The unique identifier of the object.</param>
+        /// <returns>True if the object exists and is being populated successfully</returns>
+        protected override Profile DataSelect(string id)
+        {
+            Profile profile = GetProfile(id);
+            return profile;
+        }
+
+        /// <summary>
+        /// Updates the object in its data store.
+        /// </summary>
+        protected override void DataUpdate()
+        {
+            SaveProfile(this);
+        }
+
+        /// <summary>
+        /// Inserts a new object to the data store.
+        /// </summary>
+        protected override void DataInsert()
+        {
+            SaveProfile(this);
+        }
+
+        /// <summary>
+        /// Deletes the object from the data store.
+        /// </summary>
+        protected override void DataDelete()
+        {
+            string path = BlogSettings.Instance.StorageLocation + userName.ToLowerInvariant() + ".xml";
+            if (IsDeleted)
+            {
+                try
+                {
+                    File.Delete(path);
+                    Dispose();
+                }
+                catch (FileNotFoundException)
+                {
+                    throw new FileNotFoundException("File was not found", path);
+                }
+                catch (FieldAccessException e)
+                {
+                    throw new FieldAccessException("File was not found", e.InnerException);
+                }
+            }
+        }
+
+        #endregion
     }
-
-
 }
