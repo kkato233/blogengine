994f0f2c8d3fcabce559354a51665ae401ba40a4
SND\rclarkson_cp <SND\rclarkson_cp@localhost>
2008-04-01 15:58:29 +0000

    Some minor changes to profile.

--- BlogEngine/BlogEngine.NET/admin/tinyMCE.ascx.cs
+++ BlogEngine/BlogEngine.NET/admin/tinyMCE.ascx.cs
@@ -17,15 +17,4 @@ public partial class admin_tinyMCE : System.Web.UI.UserControl
     set { txtContent.TabIndex = value; }
   }
 
-    public Unit Width
-    {
-        get { return txtContent.Width; }
-        set { txtContent.Width = value; }
-    }
-
-    public Unit Height
-    {
-        get { return txtContent.Height; }
-        set { txtContent.Height = value; }
-    }
 }
