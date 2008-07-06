eebd04ebb38ddb811d59897e9a82a488aea0576e
madskristensen <madskristensen@localhost>
2008-07-06 12:20:10 +0000

    Fixed the calendar widget and some other items from the issue tracker. Also improved the speed of TinyMCE. (1.4.0.5)

--- BlogEngine/BlogEngine.NET/App_Code/Controls/PostCalendar.cs
+++ BlogEngine/BlogEngine.NET/App_Code/Controls/PostCalendar.cs
@@ -13,11 +13,11 @@ using BlogEngine.Core;
 
 namespace Controls
 {
-  /// <summary>
-  /// Summary description for Calendar
-  /// </summary>
-  public class PostCalendar : Calendar, ICallbackEventHandler
-  {
+	/// <summary>
+	/// Summary description for Calendar
+	/// </summary>
+	public class PostCalendar : Calendar, ICallbackEventHandler
+	{
 
 		protected override void OnLoad(EventArgs e)
 		{
@@ -25,45 +25,45 @@ namespace Controls
 			base.OnLoad(e);
 		}
 
-    protected override void OnPreRender(EventArgs e)
-    {
-      if (!Page.IsCallback && !Page.IsPostBack)
-        base.VisibleDate = DateTime.Now;
+		protected override void OnPreRender(EventArgs e)
+		{
+			if (!Page.IsCallback && !Page.IsPostBack)
+				base.VisibleDate = DateTime.Now;
 
-      if (!Page.IsPostBack && Context.Request.QueryString["date"] != null)
-      {
-        DateTime date = DateTime.Now;
-        if (DateTime.TryParse(Context.Request.QueryString["date"], out date))
-          base.VisibleDate = date;
-      }
-			
-      base.OnPreRender(e);
-      if (!ShowPostTitles)
-        ShowTitle = false;
-    }
+			if (!Page.IsPostBack && Context.Request.QueryString["date"] != null)
+			{
+				DateTime date = DateTime.Now;
+				if (DateTime.TryParse(Context.Request.QueryString["date"], out date))
+					base.VisibleDate = date;
+			}
 
-    public bool ShowPostTitles
-    {
-      get { return (bool)(ViewState["ShowPostTitles"] ?? default(bool)); }
-      set { ViewState["ShowPostTitles"] = value; }
-    }
+			base.OnPreRender(e);
+			if (!ShowPostTitles)
+				ShowTitle = false;
+		}
 
-    protected override void OnDayRender(TableCell cell, CalendarDay day)
-    {
+		public bool ShowPostTitles
+		{
+			get { return (bool)(ViewState["ShowPostTitles"] ?? default(bool)); }
+			set { ViewState["ShowPostTitles"] = value; }
+		}
+
+		protected override void OnDayRender(TableCell cell, CalendarDay day)
+		{
 			if (day.IsToday)
 			{
 				cell.Attributes["id"] += "today";
 			}
 
-      List<Post> list = Post.GetPostsByDate(day.Date, day.Date);
-      if (list.Count > 0)
-      {
-        cell.Controls.Clear();
-        if (ShowPostTitles)
-        {
-          cell.Controls.Add(new LiteralControl(day.DayNumberText));
-          foreach (Post post in list)
-          {
+			List<Post> list = Post.GetPostsByDate(day.Date, day.Date);
+			if (list.Count > 0)
+			{
+				cell.Controls.Clear();
+				if (ShowPostTitles)
+				{
+					cell.Controls.Add(new LiteralControl(day.DayNumberText));
+					foreach (Post post in list)
+					{
 						if (post.IsVisible)
 						{
 							HtmlAnchor a = new HtmlAnchor();
@@ -71,15 +71,15 @@ namespace Controls
 							a.HRef = post.RelativeLink.ToString();
 							cell.Controls.Add(a);
 						}
-          }
-        }
-        else
-        {
+					}
+				}
+				else
+				{
 					if (list[0].IsVisible)
 					{
 						HtmlAnchor a = new HtmlAnchor();
 						a.InnerHtml = day.DayNumberText;
-						a.HRef = Utils.RelativeWebRoot  + day.Date.Year + "/" + day.Date.ToString("MM") + "/" + day.Date.ToString("dd") + "/default" + BlogSettings.Instance.FileExtension;
+						a.HRef = Utils.RelativeWebRoot + day.Date.Year + "/" + day.Date.ToString("MM") + "/" + day.Date.ToString("dd") + "/default" + BlogSettings.Instance.FileExtension;
 						a.Attributes["class"] = "exist";
 						cell.Controls.Add(a);
 					}
@@ -87,105 +87,112 @@ namespace Controls
 					{
 						cell.Text = day.DayNumberText;
 					}
-        }
-      }
-      else
-      {
-        cell.Controls.Clear();
-        cell.Text = day.DayNumberText;
-      }
-    }
+				}
+			}
+			else
+			{
+				cell.Controls.Clear();
+				cell.Text = day.DayNumberText;
+			}
+		}
 
-    protected override void Render(HtmlTextWriter writer)
-    {
-      if (!ShowPostTitles)
-      {
-        if ((Page.IsPostBack && !Page.IsCallback) || VisibleDate == DateTime.MinValue)
-          VisibleDate = DateTime.Now;
+		protected override void Render(HtmlTextWriter writer)
+		{
+			if (!ShowPostTitles)
+			{
+				if ((Page.IsPostBack && !Page.IsCallback) || VisibleDate == DateTime.MinValue)
+					VisibleDate = DateTime.Now;
 
-        writer.Write("<div id=\"calendarContainer\">");
-        writer.Write("<table class=\"calendar\" summary=\"\" style=\";border-collapse:collapse;\">");
-        writer.Write("<tr><td>");
+				writer.Write("<div id=\"calendarContainer\">");
+				writer.Write("<table class=\"calendar\" summary=\"\" style=\";border-collapse:collapse;\">");
+				writer.Write("<tr><td>");
 
-        DateTime oldest = GetOldestPostDate();
+				DateTime oldest = GetOldestPostDate();
 
-        if (VisibleDate.Year != oldest.Year || VisibleDate.Month != oldest.Month)
-          writer.Write("<a href=\"javascript:CalNav('" + this.VisibleDate.AddMonths(-1).ToString("yyyy-MM-dd") + "')\">" + HttpUtility.HtmlEncode(PrevMonthText) + "</a>&nbsp;&nbsp;");
-        else
-          writer.Write(HttpUtility.HtmlEncode(PrevMonthText) + "&nbsp;&nbsp;");
+				if (VisibleDate.Year != oldest.Year || VisibleDate.Month != oldest.Month)
+					writer.Write("<a href=\"javascript:CalNav('" + this.VisibleDate.AddMonths(-1).ToString("yyyy-MM-dd") + "')\">" + HttpUtility.HtmlEncode(PrevMonthText) + "</a>&nbsp;&nbsp;");
+				else
+					writer.Write(HttpUtility.HtmlEncode(PrevMonthText) + "&nbsp;&nbsp;");
 
-        writer.Write("</td><td style=\"text-align:center;width:100px\">" + VisibleDate.ToString("MMMM yyyy") + "</td><td align=\"right\">");
+				writer.Write("</td><td style=\"text-align:center;width:100px\">" + VisibleDate.ToString("MMMM yyyy") + "</td><td align=\"right\">");
 
-        if (VisibleDate.Year != DateTime.Now.Year || VisibleDate.Month != DateTime.Now.Month)
-          writer.Write("&nbsp;&nbsp;<a href=\"javascript:CalNav('" + this.VisibleDate.AddMonths(1).ToString("yyyy-MM-dd") + "')\">" + HttpUtility.HtmlEncode(NextMonthText) + "</a>");
-        else
-          writer.Write("&nbsp;&nbsp;" + HttpUtility.HtmlEncode(NextMonthText));
+				if (VisibleDate.Year != DateTime.Now.Year || VisibleDate.Month != DateTime.Now.Month)
+					writer.Write("&nbsp;&nbsp;<a href=\"javascript:CalNav('" + this.VisibleDate.AddMonths(1).ToString("yyyy-MM-dd") + "')\">" + HttpUtility.HtmlEncode(NextMonthText) + "</a>");
+				else
+					writer.Write("&nbsp;&nbsp;" + HttpUtility.HtmlEncode(NextMonthText));
 
-        writer.Write("</td></tr>");
-        writer.Write("</table>");
+				writer.Write("</td></tr>");
+				writer.Write("</table>");
 
-        base.Attributes.Add("summary", "Post calendar");
-        base.Render(writer);
-        
-        writer.Write("</div>");
+				base.Attributes.Add("summary", "Post calendar");
+				try
+				{
+					base.Render(writer);
+				}
+				catch (Exception)
+				{
+					writer.Write("<a href=\"javascript:void(location.reload(true))\">Reload page</a>");
+				}
 
-        if (!Page.IsCallback)
-          writer.Write(Script());
-      }
-      else if (!Page.IsCallback)
-      {
-        base.Render(writer);
-      }
-    }
+				writer.Write("</div>");
 
-    private DateTime GetOldestPostDate()
-    {
-      if (Post.Posts.Count > 0)
-      {
-        return Post.Posts[Post.Posts.Count - 1].DateCreated;
-      }
+				if (!Page.IsCallback)
+					writer.Write(Script());
+			}
+			else if (!Page.IsCallback)
+			{
+				base.Render(writer);
+			}
+		}
 
-      return DateTime.Now;
-    }
+		private DateTime GetOldestPostDate()
+		{
+			if (Post.Posts.Count > 0)
+			{
+				return Post.Posts[Post.Posts.Count - 1].DateCreated;
+			}
 
-    private string Script()
-    {
-      StringBuilder sb = new StringBuilder();
-      sb.Append("<script type=\"text/javascript\">");
-      sb.Append("var months = new Object();");
-      sb.Append("function CalNav(date){");
-      sb.Append("if (months[date] == null || months[date] == 'undefined')");
-      sb.Append("{" + Page.ClientScript.GetCallbackEventReference(this, "date", "UpdateCalendar", "date") + "}");
-      sb.Append("else {UpdateCalendar(months[date], date)}");
-      sb.Append("}");
-      sb.Append("</script>");
-      return sb.ToString();
-    }
+			return DateTime.Now;
+		}
 
-    #region ICallbackEventHandler Members
+		private string Script()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("<script type=\"text/javascript\">");
+			sb.Append("var months = new Object();");
+			sb.Append("function CalNav(date){");
+			sb.Append("if (months[date] == null || months[date] == 'undefined')");
+			sb.Append("{" + Page.ClientScript.GetCallbackEventReference(this, "date", "UpdateCalendar", "date") + "}");
+			sb.Append("else {UpdateCalendar(months[date], date)}");
+			sb.Append("}");
+			sb.Append("</script>");
+			return sb.ToString();
+		}
 
-    private string _Callback;
+		#region ICallbackEventHandler Members
 
-    public string GetCallbackResult()
-    {
-      return _Callback;
-    }
+		private string _Callback;
 
-    public void RaiseCallbackEvent(string eventArgument)
-    {
-      DateTime date = DateTime.Now;
-      if (DateTime.TryParse(eventArgument, out date))
-      {
-        base.VisibleDate = date;
-        base.ShowTitle = false;
-        using (System.IO.StringWriter sw = new System.IO.StringWriter())
-        {
-          this.RenderControl(new HtmlTextWriter(sw));
-          _Callback = sw.ToString();
-        }
-      }
-    }
+		public string GetCallbackResult()
+		{
+			return _Callback;
+		}
 
-    #endregion
-  }
+		public void RaiseCallbackEvent(string eventArgument)
+		{
+			DateTime date = DateTime.Now;
+			if (DateTime.TryParse(eventArgument, out date))
+			{
+				base.VisibleDate = date;
+				base.ShowTitle = false;
+				using (System.IO.StringWriter sw = new System.IO.StringWriter())
+				{
+					this.RenderControl(new HtmlTextWriter(sw));
+					_Callback = sw.ToString();
+				}
+			}
+		}
+
+		#endregion
+	}
 }
