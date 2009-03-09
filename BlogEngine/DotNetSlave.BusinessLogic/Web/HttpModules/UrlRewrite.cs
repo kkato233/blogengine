11d70d02738ea8e21caa5a494636b5bcc6427c18
rusvdw <rusvdw@localhost>
2009-03-09 17:35:35 +0000

    Fixed UrlRewriting and bin folder for Mono

--- BlogEngine/DotNetSlave.BusinessLogic/Web/HttpModules/UrlRewrite.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Web/HttpModules/UrlRewrite.cs
@@ -49,6 +49,9 @@ namespace BlogEngine.Core.Web.HttpModules
 			string path = context.Request.Path.ToUpperInvariant();
 			string url = context.Request.RawUrl.ToUpperInvariant();
 
+            path = path.Replace(".ASPX.CS", "");
+            url = url.Replace(".ASPX.CS", "");
+
 			if (url.Contains(BlogSettings.Instance.FileExtension.ToUpperInvariant()) && !url.Contains("ERROR404.ASPX"))
 			{
 				if (path == Utils.RelativeWebRoot.ToUpperInvariant() + "DEFAULT.ASPX" && context.Request.QueryString.Count == 0)
@@ -161,30 +164,28 @@ namespace BlogEngine.Core.Web.HttpModules
 			if (string.IsNullOrEmpty(context.Request.QueryString["page"]))
 				page = null;
 
-			if (YEAR_MONTH_DAY.IsMatch(url))
-			{
-				Match match = YEAR_MONTH_DAY.Match(url);
-				string year = match.Groups[1].Value;
-				string month = match.Groups[2].Value;
-				string day = match.Groups[3].Value;
-				string date = year + "-" + month + "-" + day;
-				context.RewritePath(Utils.RelativeWebRoot + "default.aspx?date=" + date + page, false);
-			}
-			else if (YEAR_MONTH.IsMatch(url))
-			{
-				Match match = YEAR_MONTH.Match(url);
-				string year = match.Groups[1].Value;
-				string month = match.Groups[2].Value;
-				string path = string.Format("default.aspx?year={0}&month={1}", year, month);
-				context.RewritePath(Utils.RelativeWebRoot + path + page, false);
-			}
-			else if (YEAR.IsMatch(url))
-			{
-				Match match = YEAR.Match(url);
-				string year = match.Groups[1].Value;
-				string path = string.Format("default.aspx?year={0}", year);
-				context.RewritePath(Utils.RelativeWebRoot + path + page, false);
-			}
+            if (YEAR_MONTH_DAY.IsMatch(url)) {
+                Match match = YEAR_MONTH_DAY.Match(url);
+                string year = match.Groups[1].Value;
+                string month = match.Groups[2].Value;
+                string day = match.Groups[3].Value;
+                string date = year + "-" + month + "-" + day;
+                context.RewritePath(Utils.RelativeWebRoot + "default.aspx?date=" + date + page, false);
+            } else if (YEAR_MONTH.IsMatch(url)) {
+                Match match = YEAR_MONTH.Match(url);
+                string year = match.Groups[1].Value;
+                string month = match.Groups[2].Value;
+                string path = string.Format("default.aspx?year={0}&month={1}", year, month);
+                context.RewritePath(Utils.RelativeWebRoot + path + page, false);
+            } else if (YEAR.IsMatch(url)) {
+                Match match = YEAR.Match(url);
+                string year = match.Groups[1].Value;
+                string path = string.Format("default.aspx?year={0}", year);
+                context.RewritePath(Utils.RelativeWebRoot + path + page, false);
+            } else {
+                context.RewritePath(url.Replace("Default.aspx", "default.aspx")); // fixes a casing oddity on Mono
+            }
+              
 		}
 
 		/// <summary>
