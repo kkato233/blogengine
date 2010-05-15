d1eb8ba6dc5b9dd69a643ba19eb98c188edc7d59
rtur <rtur.net@gmail.com>
2010-05-15 14:11:50 -0500

    Converted to VS 2010 / .NET 3.5 (1.6.1.1)

--- BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Security;
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: PermissionSet(SecurityAction.RequestMinimum, Name = "Nothing")]
 
-[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.1.1")]
