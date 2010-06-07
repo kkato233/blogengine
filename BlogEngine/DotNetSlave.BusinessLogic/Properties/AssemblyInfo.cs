ae8bc89375de77a09453c08bbc36598e06ce21ff
rtur <rtur@localhost>
2010-06-06 21:23:36 -0500

    Version number incremented (1.6.1.8)

--- BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Security;
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: PermissionSet(SecurityAction.RequestMinimum, Name = "Nothing")]
 
-[assembly: AssemblyVersion("1.6.1.7")]
+[assembly: AssemblyVersion("1.6.1.8")]
