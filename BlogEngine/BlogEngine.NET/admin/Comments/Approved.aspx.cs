c265dae9b20aca1ae1a0ceeb7a996a5125b86d25
rossisdead <rossisdead@localhost>
2010-10-07 19:21:09 -0400

    Fixed a bug in ProcessSelection where it was querying *all* the checked checkboxes on the page and then trying to parse them all without checking to see if they were corresponding to comments.
    I also cleaned up the function so that it's more efficient the post-ajax processing(it stores the comment counts to a local variable now while it's processing instead of repeatedly rewriting it to the DOM).
    
    Other misc clean up of some functions for efficiency's sake.

---
+++ BlogEngine/BlogEngine.NET/admin/Comments/Approved.aspx.cs
@@ -0,0 +1,59 @@
+﻿namespace admin.Comments
+{
+    using System;
+    using System.Collections;
+    using System.Web.Services;
+
+    using BlogEngine.Core;
+    using BlogEngine.Core.Json;
+
+    public partial class Approved : System.Web.UI.Page
+    {
+        [WebMethod]
+        public static IEnumerable LoadComments(int PageSize, int Page)
+        {
+            return JsonComments.GetComments(CommentType.Approved, PageSize, Page);
+        }
+
+        [WebMethod]
+        public static string LoadPager(int PageSize, int Page)
+        {
+            return JsonComments.GetPager(PageSize, Page, "Approved.aspx");
+        }
+
+        [WebMethod]
+        public static JsonComment GetComment(string id)
+        {
+            return JsonComments.GetComment(new System.Guid(id));
+        }
+
+        [WebMethod]
+        public static JsonComment SaveComment(string[] vals)
+        {
+            Guid gId = new Guid(vals[0]);         
+            string author = vals[1];
+            string email = vals[2];
+            string website = vals[3];
+            string cont = vals[4];
+
+            foreach (Post p in Post.Posts.ToArray())
+            {
+                foreach (Comment c in p.Comments.ToArray())
+                {
+                    if (c.Id == gId)
+                    {
+                        c.Author = author;
+                        c.Email = email;
+                        c.Website = string.IsNullOrEmpty(website) ? null : new Uri(website);
+                        c.Content = cont;
+
+                        p.Save();
+                        return JsonComments.GetComment(gId);
+                    }
+                }
+            }
+
+            return new JsonComment();
+        }
+    }
+}
---
+++ BlogEngine/BlogEngine.NET/admin/Comments/Pingbacks.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/admin.master" AutoEventWireup="true" CodeFile="Pingbacks.aspx.cs" Inherits="admin.Comments.Pingbacks" %>
+<%@ Register src="Menu.ascx" tagname="TabMenu" tagprefix="menu" %>
+<%@ Import Namespace="BlogEngine.Core" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphAdmin" Runat="Server"> 
+    <script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js"></script>
+
+    <script type="text/javascript">
+        LoadComments(1, 'Pingbacks.aspx');
+    </script>
+     
+    <div class="content-box-hdr">
+        <span class="SectionHeader">Pingbacks & Trackbacks</span>
+    </div>
+	<div class="content-box-outer">
+		<div class="content-box-right">
+			<menu:TabMenu ID="TabMenu" runat="server" />
+		</div>
+		<div class="content-box-left">
+            <div id="Container"></div>
+            <div id="Pager"></div>
+		</div>
+        <div class="action_buttons">
+		    <input type="submit" class="btnAddNew btn rounded" value="Delete Selected" onclick="return ProcessSelected('Delete', 'Pingback');" />&nbsp;
+            <span class="loader">&nbsp;</span>
+        </div>
+	</div>      
+</asp:Content>
