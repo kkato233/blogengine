f6b6c3dc3d76a408b4a611dda8e07ade390b1d00
rossisdead <rossisdead@localhost>
2010-10-15 17:11:52 -0400

    Added an additional setting for limiting the file size of remote file downloads.

---
+++ BlogEngine/BlogEngine.NET/page.aspx.cs
@@ -0,0 +1,233 @@
+﻿#region Using
+
+using System;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Web.UI;
+
+using BlogEngine.Core;
+using BlogEngine.Core.Web.Controls;
+
+using Resources;
+
+using Page = BlogEngine.Core.Page;
+
+#endregion
+
+/// <summary>
+/// The page.
+/// </summary>
+public partial class page : BlogBasePage
+{
+    /// <summary>
+    /// Raises the <see cref="E:System.Web.UI.Control.Init"/> event to initialize the page.
+    /// </summary>
+    /// <param name="e">An <see cref="T:System.EventArgs"/> that contains the event data.</param>
+    protected override void OnInit(EventArgs e)
+    {
+        if (this.Request.QueryString["deletepage"] != null && this.Request.QueryString["deletepage"].Length == 36)
+        {
+            this.DeletePage(new Guid(this.Request.QueryString["deletepage"]));
+        }
+
+        if (this.Request.QueryString["id"] != null && this.Request.QueryString["id"].Length == 36)
+        {
+            this.ServePage(new Guid(this.Request.QueryString["id"]));
+            this.AddMetaTags();
+        }
+        else
+        {
+            this.Response.Redirect(Utils.RelativeWebRoot);
+        }
+
+        base.OnInit(e);
+    }
+
+    /// <summary>
+    /// Serves the page to the containing DIV tag on the page.
+    /// </summary>
+    /// <param name="id">
+    /// The id of the page to serve.
+    /// </param>
+    private void ServePage(Guid id)
+    {
+        this.Page = BlogEngine.Core.Page.GetPage(id);
+
+        if (this.Page == null || (!this.Page.IsVisible))
+        {
+            this.Response.Redirect(string.Format("{0}error404.aspx", Utils.RelativeWebRoot), true);
+            return; // WLF: ReSharper is stupid and doesn't know that redirect returns this method.... or does it not...?
+        }
+
+        this.h1Title.InnerHtml = this.Page.Title;
+
+        var arg = new ServingEventArgs(this.Page.Content, ServingLocation.SinglePage);
+        BlogEngine.Core.Page.OnServing(this.Page, arg);
+
+        if (arg.Cancel)
+        {
+            this.Response.Redirect("error404.aspx", true);
+        }
+
+        if (arg.Body.ToLowerInvariant().Contains("[usercontrol"))
+        {
+            this.InjectUserControls(arg.Body);
+        }
+        else
+        {
+            this.divText.InnerHtml = arg.Body;
+        }
+    }
+
+    /// <summary>
+    /// Adds the meta tags and title to the HTML header.
+    /// </summary>
+    private void AddMetaTags()
+    {
+        if (this.Page == null)
+        {
+            return;
+        }
+
+        this.Title = this.Server.HtmlEncode(this.Page.Title);
+        this.AddMetaTag("keywords", this.Server.HtmlEncode(this.Page.Keywords));
+        this.AddMetaTag("description", this.Server.HtmlEncode(this.Page.Description));
+    }
+
+    /// <summary>
+    /// Deletes the page.
+    /// </summary>
+    /// <param name="id">
+    /// The page id.
+    /// </param>
+    private void DeletePage(Guid id)
+    {
+        if (!Thread.CurrentPrincipal.Identity.IsAuthenticated)
+        {
+            return;
+        }
+
+        var page = BlogEngine.Core.Page.GetPage(id);
+        page.Delete();
+        page.Save();
+        this.Response.Redirect("~/", true);
+    }
+
+    /// <summary>
+    /// The body regex.
+    /// </summary>
+    private static readonly Regex BodyRegex = new Regex(
+        @"\[UserControl:(.*?)\]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    /// <summary>
+    /// Injects any user controls if one is referenced in the text.
+    /// </summary>
+    /// <param name="content">
+    /// The content.
+    /// </param>
+    private void InjectUserControls(string content)
+    {
+        var currentPosition = 0;
+        var thematches = BodyRegex.Matches(content);
+
+        foreach (Match mymatch in thematches)
+        {
+            if (mymatch.Index > currentPosition)
+            {
+                this.divText.Controls.Add(
+                    new LiteralControl(content.Substring(currentPosition, mymatch.Index - currentPosition)));
+            }
+
+            try
+            {
+                var all = mymatch.Groups[1].Value.Trim();
+                Control usercontrol;
+
+                if (!all.EndsWith(".ascx", StringComparison.OrdinalIgnoreCase))
+                {
+                    var index = all.IndexOf(".ascx", StringComparison.OrdinalIgnoreCase) + 5;
+                    usercontrol = this.LoadControl(all.Substring(0, index));
+
+                    var parameters = this.Server.HtmlDecode(all.Substring(index));
+                    var type = usercontrol.GetType();
+                    var paramCollection = parameters.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var param in paramCollection)
+                    {
+                        var name = param.Split('=')[0].Trim();
+                        var value = param.Split('=')[1].Trim();
+                        var property = type.GetProperty(name);
+                        property.SetValue(
+                            usercontrol,
+                            Convert.ChangeType(value, property.PropertyType, CultureInfo.InvariantCulture),
+                            null);
+                    }
+                }
+                else
+                {
+                    usercontrol = this.LoadControl(all);
+                }
+
+                this.divText.Controls.Add(usercontrol);
+            }
+            catch (Exception)
+            {
+                this.divText.Controls.Add(
+                    new LiteralControl(string.Format("ERROR - UNABLE TO LOAD CONTROL : {0}", mymatch.Groups[1].Value)));
+            }
+
+            currentPosition = mymatch.Index + mymatch.Groups[0].Length;
+        }
+
+        // Finally we add any trailing static text.
+        this.divText.Controls.Add(
+            new LiteralControl(content.Substring(currentPosition, content.Length - currentPosition)));
+    }
+
+    /// <summary>
+    ///     The Page instance to render on the page.
+    /// </summary>
+    public new Page Page;
+
+    /// <summary>
+    ///     Gets the admin links to edit and delete a page.
+    /// </summary>
+    /// <value>The admin links.</value>
+    public string AdminLinks
+    {
+        get
+        {
+            if (Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine("<div id=\"admin\">");
+                sb.AppendFormat(
+                    "<a href=\"{0}admin/Pages/Pages.aspx?id={1}\">{2}</a> | ",
+                    Utils.RelativeWebRoot,
+                    this.Page.Id,
+                    labels.edit);
+                sb.AppendFormat(
+                    "<a href=\"javascript:void(0);\" onclick=\"if (confirm('Are you sure you want to delete the page?')) location.href='?deletepage={0}'\">{1}</a>",
+                    this.Page.Id,
+                    labels.delete);
+                sb.AppendLine("</div>");
+                return sb.ToString();
+            }
+
+            return string.Empty;
+        }
+    }
+
+    /// <summary>
+    /// Gets PermaLink.
+    /// </summary>
+    public string PermaLink
+    {
+        get
+        {
+            return string.Format("{0}page.aspx?id={1}", Utils.AbsoluteWebRoot, this.Page.Id);
+        }
+    }
+}
---
+++ BlogEngine/DotNetSlave.BusinessLogic/Web/HttpHandlers/JavaScriptHandler.cs
@@ -0,0 +1,354 @@
+﻿namespace BlogEngine.Core.Web.HttpHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Security;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Web;
+    using System.Web.Caching;
+
+    using BlogEngine.Core.Web.HttpModules;
+
+    /// <summary>
+    /// Removes whitespace in all stylesheets added to the 
+    ///     header of the HTML document in site.master.
+    /// </summary>
+    public class JavaScriptHandler : IHttpHandler
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets a value indicating whether another request can use the <see cref = "T:System.Web.IHttpHandler"></see> instance.
+        /// </summary>
+        /// <value></value>
+        /// <returns>true if the <see cref = "T:System.Web.IHttpHandler"></see> instance is reusable; otherwise, false.</returns>
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Implemented Interfaces
+
+        #region IHttpHandler
+
+        /// <summary>
+        /// Enables processing of HTTP Web requests by a custom 
+        ///     HttpHandler that implements the <see cref="T:System.Web.IHttpHandler"></see> interface.
+        /// </summary>
+        /// <param name="context">
+        /// An <see cref="T:System.Web.HttpContext"></see> object that provides 
+        ///     references to the intrinsic server objects 
+        ///     (for example, Request, Response, Session, and Server) used to service HTTP requests.
+        /// </param>
+        public void ProcessRequest(HttpContext context)
+        {
+            var request = context.Request;
+            var path = request.QueryString["path"];
+
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+
+            string cacheKey = request.RawUrl;
+            string script = (string)context.Cache[cacheKey];
+
+            if (String.IsNullOrEmpty(script))
+            {
+                if (path.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                {
+                    script = RetrieveRemoteScript(path, cacheKey);
+                }
+                else
+                {
+                    script = RetrieveLocalScript(path, cacheKey);
+                }
+            }
+
+
+            if (string.IsNullOrEmpty(script))
+            {
+                return;
+            }
+
+            SetHeaders(script.GetHashCode(), context);
+            context.Response.Write(script);
+
+            if (BlogSettings.Instance.EnableHttpCompression)
+            {
+                CompressionModule.CompressResponse(context); // Compress(context);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks whether to hard minify output.
+        /// </summary>
+        /// <param name="file">The file name.</param>
+        /// <returns>Whether to hard minify output.</returns>
+        private static bool HardMinify(string file)
+        {
+            var lookfor = ConfigurationManager.AppSettings.Get("BlogEngine.HardMinify").Split(
+                new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+            return lookfor.Any(file.Contains);
+        }
+
+        /// <summary>
+        /// Call this method for any extra processing that needs to be done on a script resource before
+        /// being written to the response.
+        /// </summary>
+        /// <param name="script"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private static string ProcessScript(string script, string fileName)
+        {
+            return StripWhitespace(script, HardMinify(fileName));
+        }
+
+        /// <summary>
+        /// Retrieves the local script from the disk
+        /// </summary>
+        /// <param name="file">
+        /// The file name.
+        /// </param>
+        /// <param name="cacheKey">The key used to insert this script into the cache.</param>
+        /// <returns>
+        /// The retrieve local script.
+        /// </returns>
+        private static string RetrieveLocalScript(string file, string cacheKey)
+        {
+
+            if (StringComparer.InvariantCultureIgnoreCase.Compare(Path.GetExtension(file), ".js") != 0)
+            {
+                throw new SecurityException("No access");
+            }
+
+            var path = HttpContext.Current.Server.MapPath(file);
+
+            if (File.Exists(path))
+            {
+                string script;
+                using (var reader = new StreamReader(path))
+                {
+                    script = reader.ReadToEnd();
+                }
+
+                script = ProcessScript(script, file);
+                HttpContext.Current.Cache.Insert(cacheKey, script, new CacheDependency(path));
+                return script;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Retrieves and cached the specified remote script.
+        /// </summary>
+        /// <param name="file">
+        /// The remote URL
+        /// </param>
+        /// <param name="cacheKey">The key used to insert this script into the cache.</param>
+        /// <returns>
+        /// The retrieve remote script.
+        /// </returns>
+        private static string RetrieveRemoteScript(string file, string cacheKey)
+        {
+
+            Uri url;
+
+            if (Uri.TryCreate(file, UriKind.Absolute, out url))
+            {
+                try
+                {
+
+                    var remoteFile = new RemoteFile(url, false);
+                    string script = ProcessScript(remoteFile.GetFileAsString(), file);
+                   HttpContext.Current.Cache.Insert(cacheKey, script, null, Cache.NoAbsoluteExpiration, new TimeSpan(3, 0, 0, 0));
+                    return script;
+                }
+                catch (SocketException)
+                {
+                    // The remote site is currently down. Try again next time.
+                }
+            }
+
+            return String.Empty;
+        }
+
+
+
+        /// <summary>
+        /// This will make the browser and server keep the output
+        ///     in its cache and thereby improve performance.
+        /// </summary>
+        /// <param name="hash">
+        /// The hash number.
+        /// </param>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        private static void SetHeaders(int hash, HttpContext context)
+        {
+
+            var response = context.Response;
+
+            response.ContentType = "text/javascript";
+
+            var cache = response.Cache;
+
+            cache.VaryByHeaders["Accept-Encoding"] = true;
+            cache.SetExpires(DateTime.Now.ToUniversalTime().AddDays(7));
+            cache.SetMaxAge(new TimeSpan(7, 0, 0, 0));
+            cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+
+            var etag = string.Format("\"{0}\"", hash);
+            var incomingEtag = context.Request.Headers["If-None-Match"];
+
+            cache.SetETag(etag);
+            cache.SetCacheability(HttpCacheability.Public);
+
+            if (String.Compare(incomingEtag, etag) != 0)
+            {
+                return;
+            }
+
+            response.Clear();
+            response.StatusCode = (int)HttpStatusCode.NotModified;
+            response.SuppressContent = true;
+        }
+
+        /// <summary>
+        /// Strips the whitespace from any .css file.
+        /// </summary>
+        /// <param name="body">
+        /// The body string.
+        /// </param>
+        /// <param name="blogEngineScript">
+        /// The is Blog Engine Script.
+        /// </param>
+        /// <returns>
+        /// A string contained the whitespace-removed copy of the script.
+        /// </returns>
+        private static string StripWhitespace(string body, bool blogEngineScript)
+        {
+
+            var lines = body.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var emptyLines = new StringBuilder();
+
+            // Don't use linq for this. The previous method was using Trim() in a linq query, but since
+            // it couldn't be cached, the resulting loop had to Trim it again.
+            foreach (var s in lines)
+            {
+                var line = s.Trim();
+
+                if ((line.Length > 0) && !line.StartsWith("//"))
+                {
+                    // Use AppendLine over Append, otherwise the Regex doesn't remove everything correctly.
+                    emptyLines.AppendLine(line);
+                }
+            }
+
+            body = emptyLines.ToString();
+
+            if (blogEngineScript)
+            {
+                // mark strings and regular expressions
+                var re =
+                    new Regex(
+                        "\"(([^\"\\r\\n])|(\\\"))*\"|'[^'\\r\\n]*'|/[^/\\*](?<![/\\S]/.)([^/\\\\\\r\\n]|\\\\.)*/(?=[ig]{0,2}[^\\S])",
+                        RegexOptions.Compiled | RegexOptions.Multiline);
+
+                var m = re.Matches(body);
+                var strs = new List<string>(m.Count);
+                for (var i = 0; i < m.Count; i++)
+                {
+                    strs.Add(m[i].Value);
+
+                    // replace string and regular expression with marker
+                    // This will be inlined, so there's no real reason to use a StringBuilder here.
+                    string strRegex = "_____STRINGREGEX_" + i.ToString() + "_STRINGREGEX_____";
+                    body = re.Replace(body, strRegex, 1);
+                }
+
+                // remove line comments
+                body = Regex.Replace(body, "//.*[\r\n]", String.Empty, RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+                // remove C styles comments
+                body = Regex.Replace(body, "/\\*.*?\\*/", String.Empty, RegexOptions.Compiled | RegexOptions.Singleline);
+
+                // trim left
+                body = Regex.Replace(body, "^\\s*", String.Empty, RegexOptions.Compiled | RegexOptions.Multiline);
+
+                // trim right
+                body = Regex.Replace(body, "\\s*[\\r\\n]", "\r\n", RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+                // remove whitespace beside of left curly braced
+                body = Regex.Replace(body, "\\s*{\\s*", "{", RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+                // remove whitespace beside of right curly braced
+                body = Regex.Replace(body, "\\s*}\\s*", "}", RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+                // remove whitespace beside of coma
+                body = Regex.Replace(body, "\\s*,\\s*", ",", RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+                // remove whitespace beside of semicolon
+                body = Regex.Replace(body, "\\s*;\\s*", ";", RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+                // remove newline after keywords
+                body = Regex.Replace(
+                    body,
+                    "\\r\\n(?<=\\b(abstract|boolean|break|byte|case|catch|char|class|const|continue|default|delete|do|double|else|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|while|with)\\r\\n)",
+                    " ",
+                    RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+                // restore marked strings and regular expressions
+                for (var i = 0; i < strs.Count; i++)
+                {
+                    string strReg = "_____STRINGREGEX_" + i.ToString() + "_STRINGREGEX_____";
+                    body = Regex.Replace(body, strReg, strs[i]);
+
+                }
+            }
+            else
+            {
+                
+                body = Regex.Replace(body, @"^[\s]+|[ \f\r\t\v]+$", String.Empty);
+                body = Regex.Replace(body, @"([+-])\n\1", "$1 $1");
+                body = Regex.Replace(body, @"([^+-][+-])\n", "$1");
+                body = Regex.Replace(body, @"([^+]) ?(\+)", "$1$2");
+                body = Regex.Replace(body, @"(\+) ?([^+])", "$1$2");
+                body = Regex.Replace(body, @"([^-]) ?(\-)", "$1$2");
+                body = Regex.Replace(body, @"(\-) ?([^-])", "$1$2");
+                body = Regex.Replace(body, @"\n([{}()[\],<>/*%&|^!~?:=.;+-])", "$1");
+                body = Regex.Replace(body, @"(\W(if|while|for)\([^{]*?\))\n", "$1");
+                body = Regex.Replace(body, @"(\W(if|while|for)\([^{]*?\))((if|while|for)\([^{]*?\))\n", "$1$3");
+                body = Regex.Replace(body, @"([;}]else)\n", "$1 ");
+                body = Regex.Replace(
+                    body, @"(?<=[>])\s{2,}(?=[<])|(?<=[>])\s{2,}(?=&nbsp;)|(?<=&ndsp;)\s{2,}(?=[<])", String.Empty);
+            }
+
+            return body;
+        }
+
+
+
+        #endregion
+
+    }
+}
