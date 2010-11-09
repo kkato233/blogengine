88923065340efa7290f42c83cefc108c9d2a0a64
rtur <rtur@localhost>
2010-11-09 10:36:22 -0600

    - Syntax highlighter integrated as extension - Draft lists added for posts and pages to the dashboard

--- BlogEngine/BlogEngine.NET/admin/Dashboard.aspx.cs
+++ BlogEngine/BlogEngine.NET/admin/Dashboard.aspx.cs
@@ -1,54 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Collections.Specialized;
-using System.Web.Security;
-using BlogEngine.Core;
-
-public partial class admin_Dashboard : System.Web.UI.Page
+﻿namespace Admin
 {
-    public static int PostsPublished { get; set; }
-    public static int PagesCount { get; set; }
-    public static int CommentsAll { get; set; }
-    public static int CategoriesCount { get; set; }
-    public static int TagsCount { get; set; }
-    public static int UsersCount { get; set; }
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using System.Collections.Specialized;
+    using System.Web.Security;
+    using BlogEngine.Core;
 
-    private StringCollection CategoryList = new StringCollection();
-    private StringCollection TagList = new StringCollection();
-
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class Dashboard : System.Web.UI.Page
     {
-        var postsLinq = from posts in BlogEngine.Core.Post.Posts where posts.IsPublished = true select posts.Id;
-        PostsPublished = postsLinq.Count();
+        public static int PostsPublished { get; set; }
+        public static int PagesCount { get; set; }
+        public static int CommentsAll { get; set; }
+        public static int CategoriesCount { get; set; }
+        public static int TagsCount { get; set; }
+        public static int UsersCount { get; set; }
 
-        PagesCount = BlogEngine.Core.Page.Pages.Count();
+        private StringCollection CategoryList = new StringCollection();
+        private StringCollection TagList = new StringCollection();
 
-        CommentsAll = 0;
-        foreach (var p in Post.Posts)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            CommentsAll += p.Comments.Count;
+            var postsLinq = from posts in BlogEngine.Core.Post.Posts where posts.IsPublished == true select posts.Id;
+            PostsPublished = postsLinq.Count();
 
-            foreach (var c in p.Categories)
+            PagesCount = BlogEngine.Core.Page.Pages.Count();
+
+            CommentsAll = 0;
+            foreach (var p in Post.Posts)
             {
-                if (!CategoryList.Contains(c.Id.ToString()))
-                    CategoryList.Add(c.Id.ToString());
-            }
-            CategoriesCount = CategoryList.Count;
+                List<Comment> commentList = (from c in p.Comments where c.IsPingbackOrTrackback == false select c).ToList();
+                CommentsAll += commentList.Count;
 
-            foreach (var t in p.Tags)
+                if (!p.IsPublished)
+                {
+                    // add to post drafts
+                    DraftPosts.InnerHtml += string.Format("<li><a href=\"{0}\">{1}</a>  [<a href=\"Posts/Add_entry.aspx?id={3}\">Edit</a>]  Saved: {2} by {4}</li>", p.RelativeLink, p.Title, p.DateModified.ToShortDateString() + " at " + p.DateModified.ToShortTimeString(), p.Id, p.Author);
+                    DraftPosts.Visible = true;
+                }
+
+                foreach (var c in p.Categories)
+                {
+                    if (!CategoryList.Contains(c.Id.ToString()))
+                        CategoryList.Add(c.Id.ToString());
+                }
+                CategoriesCount = CategoryList.Count;
+
+                foreach (var t in p.Tags)
+                {
+                    if (!TagList.Contains(t))
+                        TagList.Add(t);
+                }
+                TagsCount = TagList.Count;
+
+                int uCount = 0;
+                var userCollection = Membership.Provider.GetAllUsers(0, 999, out uCount);
+                UsersCount = uCount;
+            }
+
+            foreach (var pg in BlogEngine.Core.Page.Pages)
             {
-                if (!TagList.Contains(t))
-                    TagList.Add(t);
+                if (!pg.IsPublished)
+                {
+                    // add to page drafts
+                    DraftPages.InnerHtml += string.Format("<li><a href=\"{0}\">{1}</a>  [<a href=\"Pages/Add_entry.aspx?id={3}\">Edit</a>]  Saved: {2}</li>", pg.RelativeLink, pg.Title, pg.DateModified.ToShortDateString() + " at " + pg.DateModified.ToShortTimeString(), pg.Id);
+                    DraftPages.Visible = true;
+                }
             }
-            TagsCount = TagList.Count;
 
-            int uCount = 0;
-            var userCollection = Membership.Provider.GetAllUsers(0, 999, out uCount);
-            UsersCount = uCount;
         }
     }
 }
