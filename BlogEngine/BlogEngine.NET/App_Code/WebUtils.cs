dd29b5a0d836707e71f094128013e25e512080ba
rtur <rtur@localhost>
2010-11-19 21:46:15 -0600

    - Title, description and h1 tag for account pages to make SEO toolkit happier. - MediaPlayer extension do not serve on feed - feeds can't be cast to page which broke syndication in latest releases. - Added logic to use newsletter template from stantdard theme if new theme does not implement it (issue #11991).

---
+++ BlogEngine/BlogEngine.NET/App_Code/WebUtils.cs
@@ -0,0 +1,40 @@
+﻿namespace App_Code
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using BlogEngine.Core;
+
+    public static class WebUtils
+    {
+        /// <summary>
+        /// Checks to see if the current user has the rights to access an
+        /// admin settings page.
+        /// </summary>
+        /// <param name="checkOnly">
+        /// If true, check only. If false and rights are insufficient, user
+        /// will be redirected to the login page.
+        /// </param>
+        /// <returns>True if user has sufficient rights</returns>
+        public static bool CheckRightsForAdminSettingsPage(bool checkOnly)
+        {
+            if (checkOnly)
+            {
+                return
+                    Security.IsAuthorizedTo(AuthorizationCheck.HasAll,
+                        BlogEngine.Core.Rights.AccessAdminPages,
+                        BlogEngine.Core.Rights.AccessAdminSettingsPages);
+            }
+            else
+            {
+                Security.DemandUserHasRight(AuthorizationCheck.HasAll, true,
+                    BlogEngine.Core.Rights.AccessAdminPages,
+                    BlogEngine.Core.Rights.AccessAdminSettingsPages);
+            }
+
+            return true;
+        }
+
+    }
+}
