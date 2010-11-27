5ea2c007a18f75afbe5852760775bce4e0648842
Ben Amada <ben.amada@gmail.com>
2010-11-27 13:36:40 -0700

    - Restored "Dashboard" label to English in labels.resx. (2.0.0.4)

--- BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Security.Permissions;
 [assembly: ComVisible(false)]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: PermissionSet(SecurityAction.RequestMinimum, Name = "Nothing")]
-[assembly: AssemblyVersion("2.0.0.3")]
+[assembly: AssemblyVersion("2.0.0.4")]
