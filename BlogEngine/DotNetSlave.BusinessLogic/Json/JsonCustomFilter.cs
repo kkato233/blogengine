b3302599f0a17738e8ad44162f910a25b6e7d66f
Ben Amada <ben.amada@gmail.com>
2010-12-15 00:45:50 -0700

    - New SecuritySiteMapProvider class which checks "rights" attribute in Web.sitemap nodes.  - Changed XmlSiteMapProvider in all web.config files to SecuritySiteMapProvider.  - Added "rights" to Web.sitemap nodes.  - Removed "roles" from all Web.config files.  - Added Rights/Security checks on all admin pages and web methods not already checking user rights. (2.0.0.20)

---
+++ BlogEngine/DotNetSlave.BusinessLogic/Json/JsonCustomFilter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BlogEngine.Core.Json
+{
+    /// <summary>
+    /// Custom filter
+    /// </summary>
+    public class JsonCustomFilter
+    {
+        /// <summary>
+        /// Short name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Long (class) name
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// If filter enabled
+        /// </summary>
+        public bool Enabled { get; set; }
+        /// <summary>
+        /// Number of comments checked by filter
+        /// </summary>
+        public int Checked { get; set; }
+        /// <summary>
+        /// Spam comments identified
+        /// </summary>
+        public int Spam { get; set; }
+        /// <summary>
+        /// Number of mistakes made
+        /// </summary>
+        public int Mistakes { get; set; }
+        /// <summary>
+        /// Accuracy
+        /// </summary>
+        public string Accuracy
+        {
+            get
+            {
+                try
+                {
+                    double t = double.Parse(Spam.ToString());
+                    double m = double.Parse(Mistakes.ToString());
+
+                    if (m == 0 || t == 0) return "100";
+
+                    double a = 100 - (m / t * 100);
+
+                    return String.Format("{0:0.00}", a);
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
