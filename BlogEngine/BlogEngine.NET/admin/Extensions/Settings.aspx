44f781028de2483faff58ef6d5e1e4a77b0469fd
rtur <rtur@localhost>
2011-01-20 16:31:06 -0600

    - Settings page added to new extensions section - Ectension counts and toggle disabled extensions list

---
+++ BlogEngine/BlogEngine.NET/admin/Extensions/Settings.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/admin.master" AutoEventWireup="true" CodeFile="Settings.aspx.cs" Inherits="Admin.Extensions.Settings" %>
+<%@ Reference Control = "Settings.ascx" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphAdmin" Runat="Server">
+    <div class="content-box-outer">
+		<div class="content-box-right">
+            <ul>
+                <% foreach (var x in ExtensionList())
+                   {
+                       var cls = "";
+                       if (this.Request.RawUrl.IndexOf(x.Name, StringComparison.OrdinalIgnoreCase) != -1)
+                           cls = "content-box-selected";
+
+                       if (x.Settings.Count > 0 && x.Settings[0] != null && x.ShowSettings != false)
+                           Response.Write(string.Format("<li class=\"{1}\"><a href=\"Settings.aspx?ext={0}\">{0}</a></li>", x.Name, cls));
+                   }%>
+            </ul>
+		</div>
+		<div class="content-box-left">
+            <asp:PlaceHolder ID="ucPlaceHolder" runat="server"></asp:PlaceHolder>
+            <asp:HiddenField ID="args" runat="server" />
+		</div>
+	</div>
+</asp:Content>
---
+++ BlogEngine/BlogEngine.NET/admin/Extensions/Settings.aspx.cs
@@ -0,0 +1,53 @@
+﻿namespace Admin.Extensions
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Web.UI;
+    using BlogEngine.Core;
+    using BlogEngine.Core.Web.Extensions;
+
+    public partial class Settings : System.Web.UI.Page
+    {
+        protected List<ManagedExtension> ExtensionList()
+        {
+            var extensions = ExtensionManager.Extensions.Where(x => x.Key != "MetaExtension").ToList();
+
+            extensions.Sort(
+                (e1, e2) => e1.Value.Priority == e2.Value.Priority ? string.CompareOrdinal(e1.Key, e2.Key) : e1.Value.Priority.CompareTo(e2.Value.Priority));
+
+            List<ManagedExtension> manExtensions = new List<ManagedExtension>();
+
+            foreach (KeyValuePair<string, ManagedExtension> ext in extensions)
+            {
+                var oExt = ExtensionManager.GetExtension(@ext.Key);
+                manExtensions.Add(oExt);
+            }
+            return manExtensions;
+        }
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init"/> event to initialize the page.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs"/> that contains the event data.</param>
+        protected override void OnInit(EventArgs e)
+        {
+            Security.DemandUserHasRight(BlogEngine.Core.Rights.AccessAdminPages, true);
+            UserControl uc;
+
+            var extname = this.Request.QueryString["ext"];
+
+            foreach (var setting in from x in ExtensionManager.Extensions
+                                    where x.Key == extname
+                                    from setting in x.Value.Settings
+                                    where !string.IsNullOrEmpty(setting.Name) && !setting.Hidden
+                                    select setting)
+            {
+                uc = (UserControl)this.Page.LoadControl("Settings.ascx");
+                uc.ID = setting.Name;
+                this.ucPlaceHolder.Controls.Add(uc);
+            }
+
+            base.OnInit(e);
+        }
+    }
+}
