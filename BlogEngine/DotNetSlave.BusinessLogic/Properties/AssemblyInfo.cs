2dd5dd60dfa0ef794dfcef988c2a535d5baedaba
rtur <rtur@localhost>
2011-03-26 13:09:06 -0500

    - Version increment (2.0.0.48)

--- BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Security.Permissions;
 [assembly: ComVisible(false)]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: PermissionSet(SecurityAction.RequestMinimum, Name = "Nothing")]
-[assembly: AssemblyVersion("2.0.0.47")]
+[assembly: AssemblyVersion("2.0.0.48")]
