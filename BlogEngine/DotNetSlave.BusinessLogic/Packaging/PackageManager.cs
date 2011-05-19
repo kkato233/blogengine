36d2a74792233122d8dced583c0d713393980229
rtur <rtur@localhost>
2011-05-19 00:25:46 -0500

    - Added Helpers directory to app_code for html helpers - Extensions helper moved to helpers directory - NuGet library added to "lib", strongly signed and referenced from BE Core - Packaging folder added to Core to support NuGet functionality - Created Packaging html helper (2.0.0.57)

---
+++ BlogEngine/DotNetSlave.BusinessLogic/Packaging/PackageManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuGet;
+
+namespace BlogEngine.Core.Packaging
+{
+    /// <summary>
+    /// Package manager
+    /// </summary>
+    public class PackageManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pkgType"></param>
+        /// <returns></returns>
+        public static IQueryable<IPackage> GetPackages(string pkgType)
+        {
+            var repo = PackageRepositoryFactory.Default.CreateRepository(
+                new PackageSource("http://localhost/feed/FeedService.svc", "Default"));
+
+            try
+            {
+                return repo.GetPackages();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
