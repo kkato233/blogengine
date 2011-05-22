c974d63587d5a4b6bcf7c145d8b903d7848a1623
Ben Amada <ben.amada@gmail.com>
2011-05-22 15:46:09 -0700

    - CustomIdentity change to prevent security exceptions in medium trust (discussion 242207)  - URL Rewrite fix to detect IsHomepage for virtual blog-instance paths so a Page set as the homepage is displayed.  - Adding SQL CE upgrade to 2.5 script.  - Including Port in blog instance AbsoluteWebRoot if non-default port is being used. (2.0.0.59)

--- BlogEngine/DotNetSlave.BusinessLogic/Security/CustomIdentity.cs
+++ BlogEngine/DotNetSlave.BusinessLogic/Security/CustomIdentity.cs
@@ -10,42 +10,10 @@
     using System.Reflection;
     using System.Security;
 
-    [Serializable]
-    public class CustomIdentity : IIdentity, ISerializable
-    {   
-        [SecurityCriticalAttribute]
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            // With Cassini, we need to make CustomIdentity be serializable because
-            // Cassini will cross app domain boundaries (for some reason).  When this
-            // happens, User.Identity is automatically serialized by ASP.NET so it
-            // is available in the other app domains.  This CustomIdentity type
-            // will be an unknown type in other app domains.  Here, we serialize
-            // this CustomIdentity object to a type known by other app domains --
-            // GenericIdentity (this is part of .NET).
-            // We can check if this is Cassini by seeing if context.State
-            // is 'CrossAppDomain'.  If so, we create a GenericIdentity, put
-            // our Name to it and the GenericIdentity is used.
-
-            // Other than Cassini, this object should not need to be serialized.
-            // If serialization is needed for non-Cassini cases, that needs
-            // to be implemented.
-
-            if (context.State == StreamingContextStates.CrossAppDomain)
-            {
-                GenericIdentity gi = new GenericIdentity(this.Name, this.AuthenticationType);
-                info.SetType(gi.GetType());
-
-                MemberInfo[] members = FormatterServices.GetSerializableMembers(gi.GetType());
-                object[] values = FormatterServices.GetObjectData(gi, members);
-
-                for (int i = 0; i < members.Length; i++)
-                {
-                    info.AddValue(members[i].Name, values[i]);
-                }
-            }
-        }
-
+    // Need to inherit from MarshalByRefObject to prevent runtime errors with Cassini
+    // when using a custom identity.
+    public class CustomIdentity : MarshalByRefObject, IIdentity
+    {
         public string AuthenticationType
         {
             get { return "BlogEngine.NET Custom Identity"; }
