405f39ea23a7381e907e8ebbc93163b868a3bc28
Ben Amada <ben.amada@gmail.com>
2011-06-04 15:40:11 -0700

    - Path changes to Package Installer so packages are installed in main root and not in virtual, blog-instance paths. (2.0.0.65)

---
+++ BlogEngine/DotNetSlave.BusinessLogic/Json/JsonPackages.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BlogEngine.Core.Packaging;
+
+namespace BlogEngine.Core.Json
+{
+    /// <summary>
+    /// Json Packages
+    /// </summary>
+    public class JsonPackages
+    {
+        /// <summary>
+        /// Package count
+        /// </summary>
+        public static int Count { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pkgType"></param>
+        /// <param name="page"></param>
+        /// <param name="sortOrder"></param>
+        /// <param name="searchVal"></param>
+        /// <returns></returns>
+        public static List<JsonPackage> GetPage(string pkgType, int page, PackageManager.OrderType sortOrder, string searchVal)
+        {
+            var retPkgs = new List<JsonPackage>();
+
+            var packages = PackageManager.GetPackages(pkgType, page, sortOrder, searchVal);
+
+            if(packages == null)
+            {
+                return null;
+            }
+
+            Count = packages.Count();
+
+            foreach (var p in packages)
+            {
+                var jp = new JsonPackage
+                {
+                    Id = p.Id,
+                    PackageType = pkgType,
+                    Authors = string.IsNullOrEmpty(p.Authors) ? "unknown" : p.Authors,
+                    Description = p.Description,
+                    DownloadCount = p.DownloadCount,
+                    LastUpdated = p.LastUpdated.ToString("dd MMM yyyy"),
+                    Title = p.Title,
+                    Version = p.Version,
+                    Website = p.ProjectUrl,
+                    Tags = p.Tags,
+                    IconUrl = string.IsNullOrWhiteSpace(p.IconUrl)
+                        ? Utils.ApplicationRelativeWebRoot + "Pics/imagePlaceholder.png"
+                        : "http://dnbegallery.org" + p.IconUrl
+                };
+
+                if (!string.IsNullOrWhiteSpace(p.GalleryDetailsUrl))
+                    jp.PackageUrl = "http://dnbegallery.org/cms/List/Themes/" + p.Id;
+
+                retPkgs.Add(jp);
+            }
+            return retPkgs;
+        }
+    }
+}
---
+++ BlogEngine/DotNetSlave.BusinessLogic/Packaging/FileSystem.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace BlogEngine.Core.Packaging
+{
+    /// <summary>
+    /// Class for packaging IO
+    /// </summary>
+    public class FileSystem
+    {
+        /// <summary>
+        /// Copy package files
+        /// </summary>
+        /// <param name="pkgId">Package Id</param>
+        /// <param name="version">Package Version</param>
+        public static void CopyPackageFiles(string pkgId, string version)
+        {
+            //TODO: implement also for extensions and widgets, add "lib" handling
+            var src = HttpContext.Current.Server.MapPath(Utils.ApplicationRelativeWebRoot +
+                string.Format("App_Data/packages/{0}.{1}/content", pkgId, version));
+
+            var tgt = HttpContext.Current.Server.MapPath(Utils.ApplicationRelativeWebRoot);
+
+            var source = new DirectoryInfo(src);
+            var target = new DirectoryInfo(tgt);
+
+            Utils.CopyDirectoryContents(source, target);
+        }
+
+        /// <summary>
+        /// Remove package files
+        /// </summary>
+        /// <param name="pkgId">Package Id</param>
+        /// <param name="version">Package Version</param>
+        public static void RemovePackageFiles(string pkgId, string version)
+        {
+            //TODO: implement also for extensions and widgets
+            var pkg = HttpContext.Current.Server.MapPath(Utils.ApplicationRelativeWebRoot +
+                string.Format("themes/{0}", pkgId));
+
+            if (Directory.Exists(pkg))
+                ForceDeleteDirectory(pkg); // Directory.Delete(pkg, true);
+
+            // remove package itself
+            pkg = HttpContext.Current.Server.MapPath(Utils.ApplicationRelativeWebRoot +
+                string.Format("App_Data/packages/{0}.{1}", pkgId, version));
+
+            if (Directory.Exists(pkg))
+                ForceDeleteDirectory(pkg); // Directory.Delete(pkg, true);
+        }
+
+        static void ForceDeleteDirectory(string path)
+        {
+            DirectoryInfo fol;
+            var fols = new Stack<DirectoryInfo>();
+            var root = new DirectoryInfo(path);
+            fols.Push(root);
+            while (fols.Count > 0)
+            {
+                fol = fols.Pop();
+                fol.Attributes = fol.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                foreach (DirectoryInfo d in fol.GetDirectories())
+                {
+                    fols.Push(d);
+                }
+                foreach (FileInfo f in fol.GetFiles())
+                {
+                    f.Attributes = f.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                    f.Delete();
+                }
+            }
+            root.Delete(true);
+        }
+    }
+}
