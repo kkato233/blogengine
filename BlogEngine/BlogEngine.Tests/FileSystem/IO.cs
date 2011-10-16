7ad9e4c259890930f8d2ad07a15f17bfd5061a67
rtur <rtur@localhost>
2011-10-16 15:37:56 -0500

    - Added NUnit 2.5 Nuget package - Added WatiN 2.1 Nuget package - Added BlogEngine.Test project with basic WatiN tests (2.5.0.18)

---
+++ BlogEngine/BlogEngine.Tests/FileSystem/IO.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace BlogEngine.Tests.FileSystem
+{
+    [TestFixture]
+    public class IO : BeTest
+    {
+        readonly string AppDataDir = Path.Combine(RootPath(), "App_Data");
+        readonly string AppCodeDir = Path.Combine(RootPath(), "App_Code");
+
+        [Test]
+        public void CanWriteAndReadAppDataDirectory()
+        {
+            Assert.IsTrue(WriteReadDirectory(AppDataDir));
+            Assert.IsTrue(DeleteDirectory(AppDataDir));
+        }
+
+        [Test]
+        public void CanWriteAndReadAppCodeDirectory()
+        {
+            Assert.IsTrue(WriteReadDirectory(AppCodeDir));
+            Assert.IsTrue(DeleteDirectory(AppCodeDir));
+
+            // writing into code directory will
+            // recycle application pool -
+            // wait untill application back online
+            ie.GoTo(Constants.Root);
+            Wait(35);
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+
+        }
+
+        static bool WriteReadDirectory(string dir)
+        {
+            var testDir = Path.Combine(dir, "foo");
+            var file = Path.Combine(testDir, "bar.txt");
+
+            Directory.CreateDirectory(testDir);
+
+            using (StreamWriter sw = File.CreateText(file))
+            {
+                sw.WriteLine("The test");
+            }
+
+            if (!File.Exists(file))
+                return false;
+
+            using (StreamReader sr = File.OpenText(file))
+            {
+                if (sr.ReadToEnd() != "The test\r\n")
+                    return false;
+            }
+
+            using (StreamReader sr = File.OpenText(file))
+            {
+                return (sr.ReadToEnd() == "The test\r\n");
+            }
+        }
+
+        static bool DeleteDirectory(string dir)
+        {
+            File.Delete(Path.Combine(dir, "foo/bar.txt"));
+            Directory.Delete(Path.Combine(dir, "foo"));
+
+            return !Directory.Exists(Path.Combine(dir, "foo"));
+        }
+
+        static string RootPath()
+        {
+            string s = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            s = s.Substring(0, s.IndexOf("BlogEngine.Tests"));
+            s = s.Replace(@"file:///", "");
+            s = s.Replace("/", @"\");
+            s += @"BlogEngine.NET\";
+
+            return s;
+        }
+    }
+}
---
+++ BlogEngine/BlogEngine.Tests/Posts/Post.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using WatiN.Core;
+
+namespace BlogEngine.Tests.Posts
+{
+    [TestFixture]
+    public class Post : BeTest
+    {
+        string PostId = "";
+        string TheTestPost = "The test post";
+        string TxtTitle = "ctl00_cphAdmin_txtTitle";
+
+        [SetUp]
+        public void Init()
+        {
+            Login("admin");
+        }
+
+        [Test]
+        public void CanCreatePost()
+        {
+            ie.GoTo(Constants.UrlAdminAddNewPost);
+            TypeQuickly(ie.TextField(Find.ById(TxtTitle)), TheTestPost);
+
+            // tinyMCE uses frames to simulate text area, need javascript hack as workaround
+            string js = "document.getElementById('ctl00_cphAdmin_txtContent_TinyMCE1_txtContent_ifr').contentWindow.document.body.innerHTML = 'This is WATIN test post.';";
+            ie.Eval(js);
+
+            ie.Button(Find.ById("btnSave")).Click();
+
+            SetPostId();
+
+            Assert.IsTrue(ie.ContainsText(TheTestPost));
+        }
+
+        [Test]
+        public void CanDeletePost()
+        {
+            ie.GoTo(Constants.UrlAdminPosts);
+            ie.Link(Find.ById("a-" + PostId)).Click();
+
+            // give 5 seconds for ajax method to execute
+            // and romove element from the page
+            Wait(5);
+
+            Assert.IsFalse(ie.ContainsText(TheTestPost));
+        }
+
+        void SetPostId()
+        {
+            var pos = ie.Html.IndexOf("deletepost=");
+            PostId = ie.Html.Substring(pos + 11, 36);
+        }
+    }
+}
