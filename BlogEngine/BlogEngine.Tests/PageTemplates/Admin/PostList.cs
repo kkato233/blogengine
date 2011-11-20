14d0a2c78a52c029de182fdf50eb062d6ee7f866
rtur <rtur@localhost>
2011-11-19 22:05:44 -0600

    - Refactoring in the tests (page templates added)

---
+++ BlogEngine/BlogEngine.Tests/PageTemplates/Admin/PostList.cs
@@ -0,0 +1,12 @@
+﻿using WatiN.Core;
+
+namespace BlogEngine.Tests.PageTemplates.Admin
+{
+    public class PostList : Page
+    {
+        public string Url
+        {
+            get { return Constants.Root + "/admin/Posts/Posts.aspx"; }
+        }
+    }
+}
