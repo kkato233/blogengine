9e1ae9718f5288be3899441d1500cd22d52862d7
Junxy <Junxy@Junxy-PC>
2011-11-26 12:51:51 +0800

    Updated Chinese translations.

---
+++ BlogEngine/BlogEngine.Tests/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogEngine.Tests
+{
+    public class Constants
+    {
+        // page urls
+        public static readonly string Root = "http://localhost:53265";
+
+        // login
+        public static readonly string LogOff = "Log off";
+    }
+}
---
+++ BlogEngine/BlogEngine.Tests/Posts/Post.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using WatiN.Core;
+using BlogEngine.Tests.PageTemplates.Admin;
+
+namespace BlogEngine.Tests.Posts
+{
+    [TestFixture]
+    public class Post : BeTest
+    {
+        string PostId = "";
+        string TheTestPost = "The test post";
+
+        [SetUp]
+        public void Init()
+        {
+            Login("admin");
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            var trash = ie.Page<Trash>();
+            ie.GoTo(trash.Url);
+            trash.PurgeAll.Click();
+        }
+
+        [Test]
+        public void CanCreateAndDeletePost()
+        {
+            var editPost = ie.Page<EditPost>();
+
+            ie.GoTo(editPost.Url);
+
+            TypeQuickly(editPost.PostTitle, TheTestPost);
+            
+            ie.Eval(editPost.JsHack);
+
+            editPost.Save.Click();
+
+            SetPostId();
+
+            Assert.IsTrue(ie.ContainsText(TheTestPost));
+
+            ie.GoTo(ie.Page<PostList>().Url);
+            ie.Link(Find.ById("a-" + PostId)).Click();
+
+            // give 5 seconds for ajax method to execute
+            // and romove element from the page
+            Wait(5);
+
+            Assert.IsFalse(ie.ContainsText(TheTestPost));
+        }
+
+        void SetPostId()
+        {
+            var pos = ie.Html.IndexOf("deletepost=");
+            PostId = ie.Html.Substring(pos + 11, 36);
+            System.Console.WriteLine("Post ID is: " + PostId);
+        }
+    }
+}
