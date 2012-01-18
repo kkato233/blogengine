8ae8334bd9921b24a2a15cec3a7735f943991ab6
fez <fez@Typhoon.blackmarble.co.uk>
2012-01-18 14:50:28 +0000

    Revised the code to check for punctation marks to use the same model as checking for diacritics This should cover all cases.

---
+++ BlogEngine/BlogEngine.Tests/BeTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using WatiN.Core;
+using BlogEngine.Tests.PageTemplates.Account;
+
+namespace BlogEngine.Tests
+{
+    public abstract class BeTest
+    {
+        protected IE ie = null;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            ie = new IE();
+
+            ie.Refresh();
+            ie.ClearCache();
+
+            Settings.WaitForCompleteTimeOut = 240;
+
+            // to hide IE window
+            // ie.ShowWindow(WatiN.Core.Native.Windows.NativeMethods.WindowShowStyle.Hide);
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            ie.Close();
+        }
+
+        public void Login(string user, string pwd = "")
+        {
+            if (string.IsNullOrEmpty(pwd)) 
+                pwd = user;
+
+            var login = ie.Page<Login>();
+
+            ie.GoTo(login.Url);
+
+            TypeQuickly(login.UserName, user);
+            TypeQuickly(login.Password, pwd);
+
+            login.LoginButton.Click();
+        }
+
+        public void Logout()
+        {
+            ie.GoTo(Constants.Root);
+
+            var login = ie.Page<Login>();
+            var logOffLink = login.LogoffLink;
+
+            if (logOffLink != null && logOffLink.Text == Constants.LogOff)
+            {
+                logOffLink.Click();
+            }
+        }
+
+        public static void TypeQuickly(TextField textField, string text)
+        {
+            textField.SetAttributeValue("value", text);
+        }
+
+        public void Wait(int seconds)
+        {
+            int i;
+            int.TryParse(string.Format("{0}000", seconds), out i);
+            System.Threading.Thread.Sleep(i);
+        }
+    }
+}
