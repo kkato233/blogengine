02714b7af7dce45410e450db0bd5c76369ae8cc1
Philippe <Philippe@Philippe-Icom>
2012-02-02 13:52:52 +0100

    Fixed: Can't delete post if confirm delete message translation contains single or double quotation marks (as french translation)

---
+++ BlogEngine/BlogEngine.Tests/Account/Login.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace BlogEngine.Tests.Account
+{
+    [TestFixture]
+    public class Login : BeTest
+    {
+        readonly string LoginFailedMsg = "Login failed";
+        readonly string WelcomeToBeMsg = "Welcome to BlogEngine.NET";
+
+        [SetUp]
+        public void Init()
+        {
+            Logout();
+        }
+
+        [Test]
+        public void InvalidLoginShouldFail()
+        {
+            Login("foo", "bar");
+
+            Assert.IsTrue(ie.ContainsText(LoginFailedMsg));
+        }
+
+        [Test]
+        public void ValidLoginShouldPass()
+        {
+            Login("admin");
+
+            Assert.IsTrue(ie.ContainsText(WelcomeToBeMsg));
+        }
+    }
+}
---
+++ BlogEngine/BlogEngine.Tests/Navigation/UrlRewrite.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace BlogEngine.Tests.Navigation
+{
+    [TestFixture]
+    public class UrlRewrite : BeTest
+    {
+        [Test]
+        public void CanNavigateToCategoryPage()
+        {
+            ie.GoTo(Constants.Root + "/category/BlogEngineNET.aspx");
+            ie.WaitForComplete();
+            Assert.IsTrue(ie.ContainsText("Welcome to BlogEngine.NET 2.5"));
+        }
+       
+        [Test]
+        public void CanNavigateTagPage()
+        {
+            ie.GoTo(Constants.Root + "/?tag=/welcome");
+            ie.WaitForComplete();
+            Assert.IsTrue(ie.ContainsText("Welcome to BlogEngine.NET 2.5"));
+        }
+
+        [Test]
+        public void CanNavigatePage()
+        {
+            ie.GoTo(Constants.Root + "/page/Example-page.aspx");
+            ie.WaitForComplete();
+            Assert.IsTrue(ie.ContainsText("Example page"));
+        }
+
+        [Test]
+        public void CanNavigateToCalendar()
+        {
+            ie.GoTo(Constants.Root + "/calendar/default.aspx");
+            ie.WaitForComplete();
+            Assert.IsTrue(ie.Html.Contains("widgetContainer"));
+        }
+
+        [Test]
+        public void CanNavigateToPostsByAuthor()
+        {
+            ie.GoTo(Constants.Root + "/author/Admin.aspx");
+            ie.WaitForComplete();
+            Assert.IsTrue(ie.Title.Contains("All posts by admin"));
+        }
+    }
+}
