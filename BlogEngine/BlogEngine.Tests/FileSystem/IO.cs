e376104d32abf04680b076784d1b691c057e50d4
Kassoul <Kassoul@localhost>
2012-03-09 17:25:24 +0100

    Fixed: Multiple send of a same post to each newsletter recipien

---
+++ BlogEngine/BlogEngine.Tests/FileSystem/IO.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+using System.Security.AccessControl;
+using NUnit.Framework;
+
+namespace BlogEngine.Tests.FileSystem
+{
+    [TestFixture]
+    public class IO : BeTest
+    {
+        readonly string AppDataDir = Path.Combine(RootPath(), "App_Data");
+        readonly string AppCodeDir = Path.Combine(RootPath(), "App_Code");
+
+        [Test]
+        public void CanWriteAndReadAppDataDirectory()
+        {
+            Assert.IsTrue(WriteReadDirectory(AppDataDir));
+            Assert.IsTrue(DeleteDirectory(AppDataDir));
+        }
+
+        [Test]
+        [Category("Reload")]
+        public void CanWriteAndReadAppCodeDirectory()
+        {
+            Assert.IsTrue(WriteReadDirectory(AppCodeDir));
+            Assert.IsTrue(DeleteDirectory(AppCodeDir));
+
+            // writing into code directory will
+            // recycle application pool -
+            // wait untill application back online
+            ie.GoTo(Constants.Root);
+            ie.WaitForComplete();
+            //Wait(35);
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+
+        }
+
+        static bool WriteReadDirectory(string dir)
+        {
+            var testDir = Path.Combine(dir, "foo");
+            var file = Path.Combine(testDir, "bar.txt");
+
+            Directory.CreateDirectory(testDir);
+
+            using (var sw = File.CreateText(file))
+            {
+                sw.WriteLine("The test");
+            }
+
+            if (!File.Exists(file))
+                return false;
+
+            using (var sr = File.OpenText(file))
+            {
+                if (sr.ReadToEnd() != "The test\r\n")
+                    return false;
+            }
+
+            using (var sr = File.OpenText(file))
+            {
+                return (sr.ReadToEnd() == "The test\r\n");
+            }
+        }
+
+        static bool DeleteDirectory(string dir)
+        {
+            File.Delete(Path.Combine(dir, "foo/bar.txt"));
+            Directory.Delete(Path.Combine(dir, "foo"));
+
+            return !Directory.Exists(Path.Combine(dir, "foo"));
+        }
+
+        public static void MkDir(string dir)
+        {
+            string path = Path.Combine(RootPath(), dir);
+
+            if (!Directory.Exists((path)))
+                Directory.CreateDirectory(path);
+        }
+
+        public static void DelDir(string dir)
+        {
+            string path = Path.Combine(RootPath(), dir);
+
+            if (Directory.Exists((path)))
+                Directory.Delete(path);
+        }
+
+        public static void MkFile(string file, string text)
+        {
+            var path = Path.Combine(RootPath(), file);
+
+            if(!File.Exists(path))
+            {
+                using (var sw = File.CreateText(path))
+                {
+                    sw.Write(text);
+                }
+            }
+        }
+
+        public static void DelFile(string file)
+        {
+            var path = Path.Combine(RootPath(), file);
+
+            if (File.Exists((path)))
+                File.Delete(path);
+        }
+
+        static string RootPath()
+        {
+            string s = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            s = s.Substring(0, s.IndexOf("BlogEngine.Tests"));
+            s = s.Replace(@"file:///", "");
+            s = s.Replace("/", @"\");
+            s += @"BlogEngine.NET\";
+
+            return s;
+        }
+    }
+}
---
+++ BlogEngine/BlogEngine.Tests/Posts/Post.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using WatiN.Core;
+using BlogEngine.Tests.PageTemplates.Admin;
+
+namespace BlogEngine.Tests.Posts
+{
+    [TestFixture]
+    public class Post : BeTest
+    {
+        string TheTestPost = "The test post";
+
+        [SetUp]
+        public void Init()
+        {
+            Login("admin");
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            var trash = ie.Page<Trash>();
+            ie.GoTo(trash.Url);
+            trash.PurgeAll.Click();
+        }
+
+        [Test]
+        public void CanCreateAndDeletePost()
+        {
+            var editPost = ie.Page<EditPost>();
+
+            ie.GoTo(editPost.Url);
+
+            TypeQuickly(editPost.PostTitle, TheTestPost);
+
+            // this will type test post into TinyMCE editor
+            ie.Eval(editPost.JsHack);
+
+            editPost.Save.Click();
+
+            Assert.IsTrue(ie.ContainsText(TheTestPost));
+
+            Assert.IsTrue(ie.Page<PostList>().DeletePostByTitle(TheTestPost, ie), "Could not delete created post by title");
+
+            Wait(5); // wait for ajax to complete. WaitForComplete not always working (((
+            ie.WaitForComplete();
+
+            ie.GoTo(Constants.Root);
+
+            ie.WaitForComplete();
+
+            Assert.IsFalse(ie.ContainsText(TheTestPost));
+        }
+
+    }
+}
