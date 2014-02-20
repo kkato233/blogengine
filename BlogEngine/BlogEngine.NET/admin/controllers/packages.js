c5e65b3ca598eb8a20e788ccb3e180a8fcf62d3c
rtur <rtur@localhost>
2014-02-19 21:55:00 -0600

    - More work for supporting multiple gallery feeds (2.9.2.4)

---
+++ BlogEngine/BlogEngine.NET/admin/controllers/packages.js
@@ -0,0 +1,150 @@
+﻿
+angular.module('blogAdmin').controller('CustomController', ["$rootScope", "$scope", "$location", "$filter", "dataService", function ($rootScope, $scope, $location, $filter, dataService) {
+    $scope.items = [];
+    $scope.customFields = [];
+    $scope.editId = "";
+    $scope.package = {};
+    $scope.fltr = 'extensions';
+    $scope.IsPrimary = $rootScope.SiteVars.IsPrimary == "True";
+    $scope.canEditExensions = $rootScope.SiteVars.IsAdmin == "True";
+    $scope.canInstallPackages = $rootScope.SiteVars.IsAdmin == "True" && $rootScope.SiteVars.IsPrimary == "True";
+
+    $scope.id = ($location.search()).id;
+    $scope.theme = ($location.search()).id;
+
+    $scope.selectedFeed = "http://dnbegallery.org/feed/FeedService.svc";
+    $scope.galleryFeeds = [
+	    { "OptionName": "DnbeGallery.org", "OptionValue": "http://dnbegallery.org/feed/FeedService.svc", "IsSelected": false }
+	    //,{ "OptionName": "Local feed", "OptionValue": "http://localhost/galserver/nuget", "IsSelected": false }
+    ];
+
+    if ($scope.id) {
+        $("#modal-theme-edit").modal();
+    }
+    if ($location.path().indexOf("/custom/themes") == 0) {
+        $scope.fltr = 'themes';
+    }
+    if ($location.path().indexOf("/custom/widgets") == 0) {
+        $scope.fltr = 'widgets';
+    }
+    if ($location.path().indexOf("/custom/packages") == 0) {
+        $scope.fltr = 'packages';
+    }
+
+    $scope.load = function () {
+        spinOn();
+        $scope.selectedFeedObject = selectedOption($scope.galleryFeeds, $scope.selectedFeed);
+        dataService.getItems('/api/packages', { take: 0, skip: 0, filter: $scope.fltr, order: "LastUpdated desc" })
+        .success(function (data) {
+            angular.copy(data, $scope.items);
+            gridInit($scope, $filter);
+            spinOff();
+        })
+        .error(function () {
+            toastr.error($rootScope.lbl.errorLoadingPackages);
+            spinOff();
+        });
+    }
+
+    $scope.loadCustomFields = function (id) {
+        $scope.editId = id;
+        $scope.extEditSrc = SiteVars.RelativeWebRoot + "admin/Extensions/Settings.aspx?ext=" + id + "&enb=False";
+        $scope.customFields = [];
+
+        for (var i = 0, len = $scope.items.length; i < len; i++) {
+            if ($scope.items[i].Id === id) {
+                angular.copy($scope.items[i], $scope.package);
+                if ($scope.package) {
+                    if($scope.package.SettingsUrl){
+                        $scope.extEditSrc = $scope.package.SettingsUrl.replace("~/", SiteVars.RelativeWebRoot);
+                    }
+                }
+            }
+        }
+
+        dataService.getItems('/api/customfields', { filter: 'CustomType == "THEME" && ObjectId == "' + id + '"' })
+        .success(function (data) {
+            angular.copy(data, $scope.customFields);
+            $("#modal-theme-edit").modal();
+        })
+        .error(function () {
+            toastr.error($rootScope.lbl.errorLoadingCustomFields);
+        });
+    }
+
+    $scope.relocate = function (loc) {
+        $scope.pkgLocation = loc;
+        $("#fltr-loc").removeClass("active");
+        $("#fltr-gal").removeClass("active");
+        $scope.load();
+    }
+
+    $scope.save = function () {
+        spinOn();
+
+        dataService.updateItem("/api/packages/update/foo", $scope.package)
+        .success(function (data) {
+            toastr.success($rootScope.lbl.completed);
+        })
+        .error(function () {
+            toastr.error($rootScope.lbl.failed);
+        });
+
+        dataService.updateItem("/api/customfields", $scope.customFields)
+        .success(function (data) {
+            $scope.load();
+            spinOff();
+            $("#modal-theme-edit").modal('hide');
+        })
+        .error(function () {
+            toastr.error($rootScope.lbl.updateFailed);
+            spinOff();
+            $("#modal-theme-edit").modal('hide');
+        });
+    }
+
+    $scope.processChecked = function (action) {
+        spinOn();
+        var i = $scope.items.length;
+        var checked = [];
+        while (i--) {
+            var item = $scope.items[i];
+            if (item.IsChecked === true) {
+                checked.push(item);
+            }
+        }
+        if (checked.length < 1) {
+            spinOff();
+            return false;
+        }
+        dataService.processChecked("/api/packages/processchecked/" + action, checked)
+        .success(function (data) {
+            $scope.load();
+            gridInit($scope, $filter);
+            toastr.success($rootScope.lbl.completed);
+            spinOff();
+        })
+        .error(function () {
+            toastr.error($rootScope.lbl.failed);
+            spinOff();
+        });
+    }
+
+    $scope.hasChecked = function () {
+        var i = $scope.items.length;
+        var checked = [];
+        while (i--) {
+            var item = $scope.items[i];
+            if (item.IsChecked === true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    $scope.changeFeed = function () {
+        alert($scope.selectedFeedObject.OptionValue);
+    }
+
+    $scope.load();
+}]);
---
+++ BlogEngine/BlogEngine.NET/admin/editor/pagecontroller.js
@@ -0,0 +1,136 @@
+﻿angular.module('blogAdmin').controller('PageEditorController', ["$rootScope", "$scope", "$location", "$filter", "$log", "dataService", function ($rootScope, $scope, $location, $filter, $log, dataService) {
+    $scope.id = editVars.id;
+    $scope.page = newPage;
+    $scope.lookups = [];
+    $scope.selectedParent = {};
+    $scope.fullScreen = false;
+
+    $scope.load = function () {
+        var lookupsUrl = '/api/lookups';
+        dataService.getItems(lookupsUrl)
+        .success(function (data) {
+            angular.copy(data, $scope.lookups);
+            if ($scope.id) {
+                $scope.loadPage();
+            }
+        })
+        .error(function () {
+            toastr.error("Error loading lookups");
+        });
+    }
+
+    $scope.loadPage = function () {
+        spinOn();
+        var url = '/api/pages/' + $scope.id;
+        dataService.getItems(url)
+        .success(function (data) {
+            angular.copy(data, $scope.page);
+            if ($scope.page.Parent != null) {
+                $scope.selectedParent = selectedOption($scope.lookups.PageList, $scope.page.Parent.OptionValue);
+                var x = $scope.selectedParent;
+            }
+            editorSetHtml($scope.page.Content);
+            spinOff();
+        })
+        .error(function () {
+            toastr.error($rootScope.lbl.errorLoadingPage);
+            spinOff();
+        });
+    }
+
+    $scope.save = function () {
+        if (!$('#form').valid()) {
+            return false;
+        }
+        spinOn();
+        $scope.page.Content = editorGetHtml();
+        $scope.page.Parent = $scope.selectedParent;
+
+        if ($scope.page.Slug.length == 0) {
+            $scope.page.Slug = toSlug($scope.page.Title);
+        }
+
+        if ($scope.page.Id) {
+            dataService.updateItem('/api/pages/update/foo', $scope.page)
+           .success(function (data) {
+               $scope.load();
+               toastr.success("Page updated");
+               $("#modal-form").modal('hide');
+               spinOff();
+           })
+           .error(function () {
+               toastr.error("Update failed");
+               spinOff();
+           });
+        }
+        else {
+            dataService.addItem('/api/pages', $scope.page)
+           .success(function (data) {
+               toastr.success("Page added");
+               $log.log(data);
+               if (data.Id) {
+                   angular.copy(data, $scope.page);
+                   var x = $scope.page.Id;
+               }
+               $("#modal-form").modal('hide');
+               spinOff();
+           })
+           .error(function () {
+               toastr.error("Failed adding new page");
+               spinOff();
+           });
+        }
+    }
+
+    $scope.uploadFile = function (action, files) {
+        var fd = new FormData();
+        fd.append("file", files[0]);
+
+        dataService.uploadFile("/api/upload?action=" + action, fd)
+        .success(function (data) {
+            toastr.success("Uploaded");
+            var editorHtml = editorGetHtml();
+            if (action === "image") {
+                editorSetHtml(editorHtml + '<img src=' + data + ' />');
+            }
+            if (action === "video") {
+                editorSetHtml(editorHtml + '<p>[video src=' + data + ']</p>');
+            }
+            if (action === "file") {
+                var res = data.split("|");
+                if (res.length === 2) {
+                    editorSetHtml(editorHtml + '<a href="' + res[0].replace('"', '') + '">' + res[1].replace('"', '') + '</a>');
+                }
+            }
+        })
+        .error(function () { toastr.error("Import failed"); });
+    }
+
+    $scope.load();
+
+    $(document).ready(function () {
+        $.validator.addMethod(
+            "dateFormatted",
+            function (value, element) {
+                var re = /^\d{4}-\d{1,2}-\d{1,2}\s([0-9]|[0-1][0-9]|[2][0-3]):([0-5][0-9])$/;
+                return (this.optional(element) && value == "") || re.test(value);
+            },
+            "yyyy-mm-dd hh:mm"
+        );
+        $('#form').validate({
+            rules: {
+                txtTitle: { required: true }
+            }
+        });
+    });
+}]);
+
+var newPage = {
+    "Id": "",
+    "Title": "",
+    "Content": "",
+    "DateCreated": moment().format("YYYY-MM-DD HH:mm"),
+    "Slug": "",
+    "ShowInList": true,
+    "IsPublished": true
+}
