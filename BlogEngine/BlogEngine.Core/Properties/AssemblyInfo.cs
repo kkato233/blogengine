ae3e964eff4aec8eb489c0aa9b4ffa034c1a31c3
rtur <rtur@localhost>
2014-04-26 22:07:19 -0500

    - PersionCulture.cs moved to AppCode folder (2.9.3.6)

--- BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
+++ BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Security.Permissions;
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyVersion("2.9.3.5")]
+[assembly: AssemblyVersion("2.9.3.6")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
