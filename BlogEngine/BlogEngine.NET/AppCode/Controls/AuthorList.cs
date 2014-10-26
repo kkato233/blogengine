c82cfa38deba1ca98bc1a454a24ce6b714caf8ec
kkato233 <kkato233@users.noreply.github.com>
2014-10-27 01:18:18 +0900

    Escaped UserName in AuthorList when generating Href.  This is needed if a UserName contains spaces or whatnot, in order to generate the same links that are generated on the Post.
     - Nuget.Core updated to latest version 2.8.2
     - Updater to allow restart if ran into error during upgrade
    (3.1.0.1)

--- BlogEngine/BlogEngine.NET/AppCode/Controls/AuthorList.cs
+++ BlogEngine/BlogEngine.NET/AppCode/Controls/AuthorList.cs
@@ -280,7 +280,7 @@ namespace App_Code.Controls
 
                 var anc = new HtmlAnchor
                 {
-                    HRef = string.Format("{0}{1}author/{2}{3}", Utils.ApplicationRelativeWebRoot, blogName, user.UserName, BlogConfig.FileExtension),
+                    HRef = string.Format("{0}{1}author/{2}{3}", Utils.ApplicationRelativeWebRoot, blogName, Utils.RemoveIllegalCharacters(user.UserName), BlogConfig.FileExtension),
                     InnerHtml = innerHtml,
                     Title = string.Format("Author: {0}", user.UserName)
                 };
